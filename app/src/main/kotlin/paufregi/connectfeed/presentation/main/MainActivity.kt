51836bef94345b43b43f899c5b258ed71f697ee2
Paul Ellis <paul@movio.co>
2024-12-28 19:49:11 +1300

    New routing graph

--- app/src/main/kotlin/paufregi/connectfeed/presentation/main/MainActivity.kt
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package paufregi.connectfeed.presentation.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,14 +12,16 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import paufregi.connectfeed.presentation.Route
 import paufregi.connectfeed.presentation.account.AccountScreen
+import paufregi.connectfeed.presentation.login.LoginScreen
+import paufregi.connectfeed.presentation.password.PasswordScreen
 import paufregi.connectfeed.presentation.profile.ProfileScreen
 import paufregi.connectfeed.presentation.profiles.ProfilesScreen
 import paufregi.connectfeed.presentation.quickedit.QuickEditScreen
-import paufregi.connectfeed.presentation.login.SetupScreen
 import paufregi.connectfeed.presentation.ui.theme.Theme
 import kotlin.getValue
 
@@ -38,16 +41,24 @@ class MainActivity : ComponentActivity() {
             if (state.loggedIn == false && state.showLogin != true) {
                 viewModel.showLogin()
             }
-
             Theme {
-                if (state.showLogin == true) {
-                    SetupScreen(viewModel::hideLogin)
-                } else {
-                    NavHost(navController = nav, startDestination = Route.Home) {
-                        composable<Route.Home> { QuickEditScreen(nav = nav) }
-                        composable<Route.Profiles> { ProfilesScreen(nav = nav) }
-                        composable<Route.Profile> { ProfileScreen(nav = nav) }
-                        composable<Route.Account> { AccountScreen(nav = nav) }
+                NavHost(
+                    navController = nav,
+                    startDestination = if (state.showLogin == true) Route.Auth else Route.Home
+                ) {
+                    navigation<Route.Auth>(startDestination = Route.Login) {
+                        composable<Route.Login> { LoginScreen(viewModel::hideLogin) }
+                    }
+                    navigation<Route.Home>(startDestination = Route.QuickEdit) {
+                        composable<Route.QuickEdit> { QuickEditScreen(nav = nav) }
+                        navigation<Route.Settings>(startDestination = Route.Account) {
+                            composable<Route.Account> { AccountScreen(nav = nav) }
+                            composable<Route.Password> { PasswordScreen(nav = nav) }
+                        }
+                        navigation<Route.Profiles>(startDestination = Route.ProfileList) {
+                            composable<Route.ProfileList> { ProfilesScreen(nav = nav) }
+                            composable<Route.Profile> { ProfileScreen(nav = nav) }
+                        }
                     }
                 }
             }
