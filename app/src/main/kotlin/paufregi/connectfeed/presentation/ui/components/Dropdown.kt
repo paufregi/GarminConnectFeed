959ef746c775375a60c9b29d52b1b51a14e6dbfc
Paul Ellis <paul@movio.co>
2025-01-08 06:56:08 +1300

    WIP - show more data in dropdown

--- app/src/main/kotlin/paufregi/connectfeed/presentation/ui/components/Dropdown.kt
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/ui/components/Dropdown.kt
@@ -1,10 +1,18 @@
 package paufregi.connectfeed.presentation.ui.components
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocalDrink
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -13,49 +21,82 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.unit.sp
 import paufregi.connectfeed.core.models.Activity
 import paufregi.connectfeed.core.models.ActivityType
 import paufregi.connectfeed.core.models.Course
 import paufregi.connectfeed.core.models.EventType
 import paufregi.connectfeed.core.models.Profile
+import paufregi.connectfeed.core.utils.Formatter
+import kotlin.String
 
-data class DropdownItem(
-    val text: String,
-    val leadingIcon: @Composable (() -> Unit)? = null,
+sealed interface DropdownItem {
+    val text: String
+    val leadingIcon: @Composable (() -> Unit)?
     val onClick: () -> Unit
-)
 
+    data class TextDropdownItem(
+        override val text: String,
+        override val leadingIcon: @Composable (() -> Unit)? = null,
+        override val onClick: () -> Unit
+    ) : DropdownItem
+
+    data class DistanceDropdownItem(
+        override val text: String,
+        val distance: String?,
+        override val leadingIcon: @Composable (() -> Unit)? = null,
+        override val onClick: () -> Unit
+    ) : DropdownItem
+
+    data class ProfileDropdownItem(
+        override val text: String,
+        val distance: String?,
+        val water: Boolean,
+        override val leadingIcon: @Composable (() -> Unit)? = null,
+        override val onClick: () -> Unit
+    ) : DropdownItem
+}
+
+@SuppressLint("DefaultLocale")
 @ExperimentalMaterial3Api
-fun Activity.toDropdownItem(onClick: () -> Unit) = DropdownItem(
+fun Activity.toDropdownItem(onClick: () -> Unit) = DropdownItem.DistanceDropdownItem(
     text = name,
+    distance = if (distance != null) Formatter.distance(distance) else null,
     leadingIcon = { ActivityIcon(this.type) },
     onClick = onClick
 )
 
 @ExperimentalMaterial3Api
-fun ActivityType.toDropdownItem(onClick: () -> Unit) = DropdownItem(
+fun ActivityType.toDropdownItem(onClick: () -> Unit) = DropdownItem.TextDropdownItem(
     text = name,
     leadingIcon = { ActivityIcon(this) },
     onClick = onClick
 )
 
 @ExperimentalMaterial3Api
-fun EventType.toDropdownItem(onClick: () -> Unit) = DropdownItem(
+fun EventType.toDropdownItem(onClick: () -> Unit) = DropdownItem.TextDropdownItem(
     text = name,
     onClick = onClick
 )
 
+@SuppressLint("DefaultLocale")
 @ExperimentalMaterial3Api
-fun Course.toDropdownItem(onClick: () -> Unit) = DropdownItem(
+fun Course.toDropdownItem(onClick: () -> Unit) = DropdownItem.DistanceDropdownItem(
     text = name,
+    distance = Formatter.distance(distance),
     leadingIcon = { ActivityIcon(type) },
     onClick = onClick
 )
 
+@SuppressLint("DefaultLocale")
 @ExperimentalMaterial3Api
-fun Profile.toDropdownItem(onClick: () -> Unit) = DropdownItem(
+fun Profile.toDropdownItem(onClick: () -> Unit) = DropdownItem.ProfileDropdownItem(
     text = this.name,
+    distance = if (course != null) Formatter.distance(course.distance) else null,
+    water = water != null,
     leadingIcon = { ActivityIcon(activityType) },
     onClick = onClick
 )
@@ -94,7 +135,7 @@ fun Dropdown(
         ){
             items.forEach{
                 DropdownMenuItem(
-                    text = { Text(it.text) },
+                    text = { TextDropdownMenuItem(it) },
                     leadingIcon = it.leadingIcon,
                     onClick = {
                         it.onClick()
@@ -102,6 +143,57 @@ fun Dropdown(
                     }
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun TextDropdownMenuItem(
+    item: DropdownItem
+) {
+    when (item) {
+        is DropdownItem.TextDropdownItem -> Text(item.text)
+
+        is DropdownItem.DistanceDropdownItem -> Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(item.text)
+            if (item.distance != null) Text(text = "${item.distance} km", fontSize = 10.sp)
+        }
+
+        is DropdownItem.ProfileDropdownItem -> Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(item.text)
+            Row {
+                if (item.water) { Icon(modifier = Modifier.scale(0.7f), imageVector = Icons.Default.LocalDrink, contentDescription = null) }
+                if (item.distance != null) Text(text = "${item.distance} km", fontSize = 10.sp)
+            }
+//            Column(horizontalAlignment = Alignment.End) {
+//                Row {
+//                    if (item.water) {
+//                        Icon(
+//                            modifier = Modifier.scale(0.8f),
+//                            imageVector = Icons.Default.LocalDrink,
+//                            contentDescription = null,
+//                        )
+//                    }
+//                    if (item.distance != null) {
+//                        Icon(
+//                            modifier = Modifier.scale(0.8f),
+//                            imageVector = Icons.Default.LocationOn,
+//                            contentDescription = null,
+//                        )
+//                    }
+//                }
+//                if (item.distance != null) {
+//                    Text(text = "${item.distance} km", fontSize = 10.sp)
+//                }
+//            }
         }
     }
 }
