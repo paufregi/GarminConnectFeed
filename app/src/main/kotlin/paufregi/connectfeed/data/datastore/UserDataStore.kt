79a73beaaee10a4260d5ad685491d18803051179
Paul <paulfrancis.ellis@gmail.com>
2025-01-11 00:15:15 +1300

    Datastore improvements (#59)
    * WIP - Refactor crypto
    
    * Refactor crypto object
    
    * Improve datastore store and get functions
    
    * Change encryption parameters
    
    * Revert crypto algorithm
    
    * Add CoroutineContext to runBlocking calls

--- app/src/main/kotlin/paufregi/connectfeed/data/datastore/UserDataStore.kt
+++ app/src/main/kotlin/paufregi/connectfeed/data/datastore/UserDataStore.kt
@@ -13,12 +13,10 @@ import paufregi.connectfeed.core.models.User
 import paufregi.connectfeed.data.api.models.OAuth1
 import paufregi.connectfeed.data.api.models.OAuth2
 import paufregi.connectfeed.data.api.models.OAuthConsumer
-import paufregi.connectfeed.data.keystore.CryptoManager
+import paufregi.connectfeed.data.utils.Crypto
+import java.util.Base64
 
-class UserDataStore (
-    val dataStore: DataStore<Preferences>,
-    val crypto: CryptoManager
-) {
+class UserDataStore (val dataStore: DataStore<Preferences>) {
 
     companion object {
         private val USER = byteArrayPreferencesKey("user")
@@ -28,48 +26,53 @@ class UserDataStore (
         private val OAUTH2 = byteArrayPreferencesKey("oauth2")
     }
 
-    private inline fun <reified T>getValue(key: Preferences.Key<ByteArray>): Flow<T?> =
-        dataStore.data.map { preferences ->
-            preferences[key]?.let { crypto.decrypt(it) }?.let { Json.decodeFromString<T>(it) }
+    private inline fun <reified T>get(key: Preferences.Key<ByteArray>): Flow<T?> =
+        dataStore.data.map {
+            it[key]
+                ?.let { Base64.getDecoder().decode(it) }
+                ?.let { Crypto.decrypt(it) }?.decodeToString()
+                ?.let { Json.decodeFromString(it) }
         }
 
-    private suspend inline fun <reified T>storeValue(value: T, key: Preferences.Key<ByteArray>) {
-        dataStore.edit { preferences ->
-            preferences[key] = crypto.encrypt(Json.encodeToString(value))
+    private suspend inline fun <reified T>store(value: T, key: Preferences.Key<ByteArray>) {
+        dataStore.edit {
+            it[key] = Json.encodeToString(value).toByteArray()
+                .let { Crypto.encrypt(it) }
+                .let { Base64.getEncoder().encode(it) }
         }
     }
 
-    private suspend inline fun removeValue(key: Preferences.Key<ByteArray>) {
-        dataStore.edit { preferences -> preferences.remove(key) }
+    private suspend inline fun remove(key: Preferences.Key<ByteArray>) {
+        dataStore.edit { it.remove(key) }
     }
 
-    fun getUser(): Flow<User?> = getValue<User>(USER)
+    fun getUser(): Flow<User?> = get<User>(USER)
 
-    fun getCredential(): Flow<Credential?> = getValue<Credential>(CREDENTIAL)
+    fun getCredential(): Flow<Credential?> = get<Credential>(CREDENTIAL)
 
-    fun getOAuthConsumer(): Flow<OAuthConsumer?> = getValue<OAuthConsumer>(OAUTH_CONSUMER)
+    fun getOAuthConsumer(): Flow<OAuthConsumer?> = get<OAuthConsumer>(OAUTH_CONSUMER)
 
-    fun getOauth1(): Flow<OAuth1?> = getValue<OAuth1>(OAUTH1)
+    fun getOauth1(): Flow<OAuth1?> = get<OAuth1>(OAUTH1)
 
-    fun getOauth2(): Flow<OAuth2?> = getValue<OAuth2>(OAUTH2)
+    fun getOauth2(): Flow<OAuth2?> = get<OAuth2>(OAUTH2)
 
-    suspend fun saveUser(user: User) = storeValue(user, USER)
+    suspend fun saveUser(user: User) = store(user, USER)
 
-    suspend fun saveCredential(credential: Credential) = storeValue(credential, CREDENTIAL)
+    suspend fun saveCredential(credential: Credential) = store(credential, CREDENTIAL)
 
-    suspend fun saveOAuthConsumer(consumer: OAuthConsumer) = storeValue(consumer, OAUTH_CONSUMER)
+    suspend fun saveOAuthConsumer(consumer: OAuthConsumer) = store(consumer, OAUTH_CONSUMER)
 
-    suspend fun saveOAuth1(oauth1: OAuth1) = storeValue(oauth1, OAUTH1)
+    suspend fun saveOAuth1(oauth1: OAuth1) = store(oauth1, OAUTH1)
 
-    suspend fun saveOAuth2(oauth2: OAuth2) = storeValue(oauth2, OAUTH2)
+    suspend fun saveOAuth2(oauth2: OAuth2) = store(oauth2, OAUTH2)
 
-    suspend fun deleteUser() = removeValue(USER)
+    suspend fun deleteUser() = remove(USER)
 
-    suspend fun deleteCredential() = removeValue(CREDENTIAL)
+    suspend fun deleteCredential() = remove(CREDENTIAL)
 
-    suspend fun deleteOAuthConsumer() = removeValue(OAUTH_CONSUMER)
+    suspend fun deleteOAuthConsumer() = remove(OAUTH_CONSUMER)
 
-    suspend fun deleteOAuth1() = removeValue(OAUTH1)
+    suspend fun deleteOAuth1() = remove(OAUTH1)
 
-    suspend fun deleteOAuth2() = removeValue(OAUTH2)
+    suspend fun deleteOAuth2() = remove(OAUTH2)
 }
