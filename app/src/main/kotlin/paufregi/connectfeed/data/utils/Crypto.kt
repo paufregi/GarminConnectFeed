d08c362230054e33e702217cfdf1f76e72130d0f
Paul Ellis <paul@movio.co>
2025-01-10 23:56:05 +1300

    Revert crypto algorithm

--- app/src/main/kotlin/paufregi/connectfeed/data/utils/Crypto.kt
+++ app/src/main/kotlin/paufregi/connectfeed/data/utils/Crypto.kt
@@ -10,14 +10,15 @@ import javax.crypto.spec.IvParameterSpec
 
 object Crypto {
     private const val ALIAS = "ConnectFeedKey"
+    private const val ANDROID_KEY_STORE = "AndroidKeyStore"
     private const val ALGORITHM = KeyProperties.KEY_ALGORITHM_AES
-    private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_GCM
-    private const val PADDING = KeyProperties.ENCRYPTION_PADDING_NONE
+    private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_CBC
+    private const val PADDING = KeyProperties.ENCRYPTION_PADDING_PKCS7
     private const val TRANSFORMATION = "$ALGORITHM/$BLOCK_MODE/$PADDING"
 
     private val cipher = Cipher.getInstance(TRANSFORMATION)
     private val keyStore = KeyStore
-        .getInstance("AndroidKeyStore")
+        .getInstance(ANDROID_KEY_STORE)
         .apply { load(null) }
 
     private fun getKey(): SecretKey {
