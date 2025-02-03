f29b8efb654722177f3d74b3805e554e62cb9e9f
Paul Ellis <paul@movio.co>
2025-02-03 14:25:16 +1300

    Strava auth use cases

---
+++ app/src/test/kotlin/paufregi/connectfeed/core/usecases/DisconnectStravaTest.kt
@@ -0,0 +1,38 @@
+package paufregi.connectfeed.core.usecases
+
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.confirmVerified
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import paufregi.connectfeed.core.models.Profile
+import paufregi.connectfeed.data.repository.GarminRepository
+import paufregi.connectfeed.data.repository.StravaAuthRepository
+
+class DisconnectStravaTest {
+
+    private val repo = mockk<StravaAuthRepository>()
+    private lateinit var useCase: DisconnectStrava
+
+    @Before
+    fun setup(){
+        useCase = DisconnectStrava(repo)
+    }
+
+    @After
+    fun tearDown(){
+        clearAllMocks()
+    }
+
+    @Test
+    fun `Delete profile`() = runTest {
+        coEvery { repo.clear() } returns Unit
+        useCase()
+        coVerify { repo.clear() }
+        confirmVerified(repo)
+    }
+}
