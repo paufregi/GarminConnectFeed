fadc192f2cf0da2a9c84d1d1cd5f85a22186b2f6
Paul Ellis <paul@movio.co>
2025-02-03 14:25:16 +1300

    Strava auth use cases

---
+++ app/src/main/kotlin/paufregi/connectfeed/core/usecases/IsStravaLoggedIn.kt
@@ -0,0 +1,13 @@
+package paufregi.connectfeed.core.usecases
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import paufregi.connectfeed.data.repository.StravaAuthRepository
+import javax.inject.Inject
+
+class IsStravaLoggedIn @Inject constructor (
+    private val authRepository: StravaAuthRepository
+) {
+    operator fun invoke(): Flow<Boolean> =
+        authRepository.getCode().map { it != null }
+}
