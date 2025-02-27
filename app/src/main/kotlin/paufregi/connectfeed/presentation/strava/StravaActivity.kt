68141994234b4dc8538addcb38819fb2d51e7a69
Paul Ellis <paul@movio.co>
2025-02-27 17:09:56 +1300

    Strava activity to handle redirect from Strava auth

--- app/src/main/kotlin/paufregi/connectfeed/presentation/strava/StravaActivity.kt
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/strava/StravaActivity.kt
@@ -6,21 +6,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.unit.dp
-import paufregi.connectfeed.presentation.ui.components.SimpleScaffold
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import dagger.hilt.android.AndroidEntryPoint
 import paufregi.connectfeed.presentation.ui.theme.Theme
 import kotlin.getValue
 
+@AndroidEntryPoint
 @ExperimentalMaterial3Api
 class StravaActivity: ComponentActivity() {
     private val viewModel: StravaViewModel by viewModels()
@@ -30,28 +23,18 @@ class StravaActivity: ComponentActivity() {
         enableEdgeToEdge()
         val code = intent.data?.getQueryParameter("code")
 
+        Log.i("StravaActivity", "code: $code")
+
         code?.let { viewModel.saveCode(it) }
 
         setContent {
+            val state by viewModel.state.collectAsStateWithLifecycle()
             Theme {
-                SimpleScaffold { pd ->
-                    Column(
-                        verticalArrangement = Arrangement.spacedBy(
-                            16.dp,
-                            Alignment.CenterVertically
-                        ),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier
-                            .padding(pd)
-                            .fillMaxSize()
-                            .padding(horizontal = 20.dp)
-                            .testTag("account_form")
-                    ) {
-                        TextField(value=code ?: "", onValueChange = {}, label = { Text("Strava OAuth") })
-                    }
-                }
+                StravaScreen(
+                    state = state,
+                    onComplete = { finish() }
+                )
             }
-
         }
     }
 
