f9ed1156088436e04fe540e0c1dda8da89d56d1a
Paul Ellis <paul@movio.co>
2025-03-04 17:08:27 +1300

    Remove core.StravaActivity as not necessary

--- app/src/main/kotlin/paufregi/connectfeed/core/models/StravaActivity.kt
+++
@@ -1,9 +0,0 @@
-package paufregi.connectfeed.core.models
-
-data class StravaActivity(
-    val id: Long,
-    val name: String,
-    val distance: Double?,
-    val externalId: String,
-    val type: ActivityType,
-)
