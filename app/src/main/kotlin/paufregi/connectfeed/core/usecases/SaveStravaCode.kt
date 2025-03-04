d8e56e8b11a79564f84757da9656e34f87d909c0
Paul Ellis <paul@movio.co>
2025-03-04 17:10:53 +1300

    Add usecase to exchange strava code and remove usecase to save strava code

--- app/src/main/kotlin/paufregi/connectfeed/core/usecases/SaveStravaCode.kt
+++
@@ -1,8 +0,0 @@
-package paufregi.connectfeed.core.usecases
-
-import paufregi.connectfeed.data.repository.StravaAuthRepository
-import javax.inject.Inject
-
-class SaveStravaCode @Inject constructor (private val stravaAuthRepository: StravaAuthRepository) {
-    suspend operator fun invoke(code: String) = stravaAuthRepository.saveCode(code)
-}
