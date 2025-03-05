e9bf4b3f86c2aaebcd471a315c36035931b241d4
Paul Ellis <paul@movio.co>
2025-03-05 15:08:53 +1300

    Add test for Strava activity and sportType conversion to core models

--- app/src/test/kotlin/paufregi/connectfeed/data/api/garmin/models/ActivityTypeTest.kt
+++ app/src/test/kotlin/paufregi/connectfeed/data/api/garmin/models/ActivityTypeTest.kt
@@ -7,126 +7,126 @@ import paufregi.connectfeed.core.models.ActivityType as CoreActivityType
 class ActivityTypeTest {
 
     @Test
-    fun `To Core activity - running`() {
+    fun `To Core activity type - running`() {
         val activityType = ActivityType(id = 1, key = "running")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Running)
     }
 
     @Test
-    fun `To Core activity - cycling`() {
+    fun `To Core activity type - cycling`() {
         val activityType = ActivityType(id = 2, key = "cycling")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Cycling)
     }
 
     @Test
-    fun `To Core activity - hiking`() {
+    fun `To Core activity type - hiking`() {
         val activityType = ActivityType(id = 3, key = "hiking")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Hiking)
     }
 
     @Test
-    fun `To Core activity - other`() {
+    fun `To Core activity type - other`() {
         val activityType = ActivityType(id = 4, key = "other")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Other)
     }
 
     @Test
-    fun `To Core activity - trail running`() {
+    fun `To Core activity type - trail running`() {
         val activityType = ActivityType(id = 6, key = "trail_running")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.TrailRunning)
     }
 
     @Test
-    fun `To Core activity - walking`() {
+    fun `To Core activity type - walking`() {
         val activityType = ActivityType(id = 9, key = "walking")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Walking)
     }
 
     @Test
-    fun `To Core activity - road cycling`() {
+    fun `To Core activity type - road cycling`() {
         val activityType = ActivityType(id = 10, key = "cycling")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Cycling)
     }
 
     @Test
-    fun `To Core activity - strength`() {
+    fun `To Core activity type - strength`() {
         val activityType = ActivityType(id = 13, key = "strength_training")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Strength)
     }
 
     @Test
-    fun `To Core activity - treadmill running`() {
+    fun `To Core activity type - treadmill running`() {
         val activityType = ActivityType(id = 18, key = "treadmill_running")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.TreadmillRunning)
     }
 
     @Test
-    fun `To Core activity - indoor cycling`() {
+    fun `To Core activity type - indoor cycling`() {
         val activityType = ActivityType(id = 25, key = "indoor_cycling")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.IndoorCycling)
     }
 
     @Test
-    fun `To Core activity - swimming`() {
+    fun `To Core activity type - swimming`() {
         val activityType = ActivityType(id = 26, key = "swimming")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Swimming)
     }
 
     @Test
-    fun `To Core activity - lap swimming`() {
+    fun `To Core activity type - lap swimming`() {
         val activityType = ActivityType(id = 27, key = "lap_swimming")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Swimming)
     }
 
     @Test
-    fun `To Core activity - open water swimming`() {
+    fun `To Core activity type - open water swimming`() {
         val activityType = ActivityType(id = 28, key = "open_water_swimming")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.OpenWaterSwimming)
     }
 
     @Test
-    fun `To Core activity - eBiking`() {
+    fun `To Core activity type - eBiking`() {
         val activityType = ActivityType(id = 176, key = "e_bike_fitness")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.EBiking)
     }
 
     @Test
-    fun `To Core activity - yoga`() {
+    fun `To Core activity type - yoga`() {
         val activityType = ActivityType(id = 163, key = "yoga")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Yoga)
     }
 
     @Test
-    fun `To Core activity - breathwork`() {
+    fun `To Core activity type - breathwork`() {
         val activityType = ActivityType(id = 164, key = "breathwork")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Breathwork)
     }
 
     @Test
-    fun `To Core activity - meditation`() {
+    fun `To Core activity type - meditation`() {
         val activityType = ActivityType(id = 202, key = "meditation")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Meditation)
     }
 
     @Test
-    fun `To Core activity - rugby`() {
+    fun `To Core activity type - rugby`() {
         val activityType = ActivityType(id = 208, key = "rugby")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Rugby)
@@ -134,21 +134,21 @@ class ActivityTypeTest {
 
 
     @Test
-    fun `To Core activity - soccer`() {
+    fun `To Core activity type - soccer`() {
         val activityType = ActivityType(id = 215, key = "soccer")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Soccer)
     }
 
     @Test
-    fun `To Core activity - jump rope`() {
+    fun `To Core activity type - jump rope`() {
         val activityType = ActivityType(id = 254, key = "jump_rope")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.JumpRope)
     }
 
     @Test
-    fun `To Core activity - unknown`() {
+    fun `To Core activity type - unknown`() {
         val activityType = ActivityType(id = 999, key = "unknown")
 
         assertThat(activityType.toCore()).isEqualTo(CoreActivityType.Unknown)
