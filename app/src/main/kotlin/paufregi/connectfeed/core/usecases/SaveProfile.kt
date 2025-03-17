de2365ff8300886e5949bfbb29d03a26a7b0e483
Paul <paulfrancis.ellis@gmail.com>
2025-03-17 15:22:51 +1300

    Swimming profile (#99)
    * Add swimming as Activity Type available in the profile creation
    
    * Add allowCourseInProfile field to ActivityTypes

--- app/src/main/kotlin/paufregi/connectfeed/core/usecases/SaveProfile.kt
+++ app/src/main/kotlin/paufregi/connectfeed/core/usecases/SaveProfile.kt
@@ -1,6 +1,5 @@
 package paufregi.connectfeed.core.usecases
 
-import paufregi.connectfeed.core.models.ActivityType
 import paufregi.connectfeed.core.models.Profile
 import paufregi.connectfeed.core.models.Result
 import paufregi.connectfeed.data.repository.GarminRepository
@@ -8,14 +7,9 @@ import javax.inject.Inject
 
 class SaveProfile @Inject constructor (private val garminRepository: GarminRepository) {
     suspend operator fun invoke(profile: Profile):Result<Unit> {
-
-        val noCourseAllowed = sequenceOf(
-            ActivityType.Any, ActivityType.Strength
-        )
-
         if (profile.name.isBlank()) return Result.Failure("Name cannot be empty")
         if (profile.course != null) {
-            if (noCourseAllowed.contains(profile.activityType)) return Result.Failure("Can't have course for ${profile.activityType.name} activity type")
+            if (!profile.activityType.allowCourseInProfile) return Result.Failure("Can't have course for ${profile.activityType.name} activity type")
             if (profile.course.type != profile.activityType) return Result.Failure("Course must match activity type")
         }
 
