8efc40b080ace353cc2287953fe02fde102ebdd6
Paul Ellis <paul@movio.co>
2025-03-24 11:36:09 +1300

    Add tests for StravaViewModel

---
+++ app/src/test/kotlin/paufregi/connectfeed/presentation/strava/StravaViewModelTest.kt
@@ -0,0 +1,80 @@
+package paufregi.connectfeed.presentation.strava
+
+import app.cash.turbine.test
+import com.google.common.truth.Truth.assertThat
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.confirmVerified
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import paufregi.connectfeed.core.models.Result
+import paufregi.connectfeed.core.usecases.StravaCodeExchange
+import paufregi.connectfeed.presentation.utils.MainDispatcherRule
+
+@ExperimentalCoroutinesApi
+class StravaViewModelTest {
+
+    private val stravaCodeExchange = mockk<StravaCodeExchange>()
+
+    private lateinit var viewModel: StravaViewModel
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @Before
+    fun setup(){
+        viewModel = StravaViewModel(stravaCodeExchange)
+    }
+
+    @After
+    fun tearDown(){
+        clearAllMocks()
+    }
+
+    @Test
+    fun `Initial state`() = runTest {
+        viewModel.state.test {
+            val state = awaitItem()
+            assertThat(state).isEqualTo(StravaState.Processing)
+            cancelAndIgnoreRemainingEvents()
+        }
+
+        confirmVerified(stravaCodeExchange)
+    }
+
+    @Test
+    fun `Exchange token`() = runTest {
+        coEvery { stravaCodeExchange(any()) } returns Result.Success(Unit)
+        viewModel.state.test {
+            var state = awaitItem() // Initial state
+            viewModel.exchangeToken("code")
+            state = awaitItem()
+            assertThat(state).isEqualTo(StravaState.Success)
+            cancelAndIgnoreRemainingEvents()
+        }
+
+        coVerify { stravaCodeExchange("code") }
+        confirmVerified(stravaCodeExchange)
+    }
+
+    @Test
+    fun `Exchange token - failure`() = runTest {
+        coEvery { stravaCodeExchange(any()) } returns Result.Failure("error")
+        viewModel.state.test {
+            var state = awaitItem() // Initial state
+            viewModel.exchangeToken("code")
+            state = awaitItem()
+            assertThat(state).isEqualTo(StravaState.Failure)
+            cancelAndIgnoreRemainingEvents()
+        }
+
+        coVerify { stravaCodeExchange("code") }
+        confirmVerified(stravaCodeExchange)
+    }
+}
