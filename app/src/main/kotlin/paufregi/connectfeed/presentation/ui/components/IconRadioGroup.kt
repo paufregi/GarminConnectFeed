a9afaf6796935288f92cecaaefb489e8101a3ddf
Paul Ellis <paul@movio.co>
2025-03-25 11:37:01 +1300

    Center icon in IconRadioGroup

--- app/src/main/kotlin/paufregi/connectfeed/presentation/ui/components/IconRadioGroup.kt
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/ui/components/IconRadioGroup.kt
@@ -50,7 +50,7 @@ fun <T> IconRadioGroup(
                     contentDescription = null,
                     tint = MaterialTheme.colorScheme.onPrimaryContainer,
                     modifier = Modifier
-                        .padding(top = 5.dp)
+                        .padding(top = 7.dp)
                         .align(Alignment.Center)
                         .size(if (isSelected) 46.dp else 38.dp)
                         .alpha(if (isSelected) 1f else 0.5f)
