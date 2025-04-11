98da84e1acdb1964fc1dec96fce4937fa1c324cc
Paul Ellis <paul@movio.co>
2025-04-11 21:54:40 +1200

    Move out the RenphoReader.read

--- app/src/test/kotlin/paufregi/connectfeed/core/usecases/SyncWeightTest.kt
+++ app/src/test/kotlin/paufregi/connectfeed/core/usecases/SyncWeightTest.kt
@@ -37,7 +37,6 @@ class SyncWeightTest {
 
     @Before
     fun setup(){
-        mockkObject(RenphoReader)
         mockkObject(Formatter)
         mockkObject(FitWriter)
 
@@ -47,19 +46,62 @@ class SyncWeightTest {
     @After
     fun tearDown(){
         clearAllMocks()
-        unmockkObject(RenphoReader)
         unmockkObject(Formatter::class)
         unmockkObject(FitWriter::class)
     }
 
     @Test
     fun `Upload file`() = runTest {
-        val csvText = """
-            Time of Measurement,Weight(kg),BMI,Body Fat(%),Fat-Free Mass(kg),Subcutaneous Fat(%),Visceral Fat,Body Water(%),Skeletal Muscle(%),Muscle Mass(kg),Bone Mass(kg),Protein(%),BMR(kcal),Metabolic Age,Optimal weight(kg),Target to optimal weight(kg),Target to optimal fat mass(kg),Target to optimal muscle mass(kg),Body Type,Remarks
-            2024-01-01 10:20:30,76.15,23.8,23.2,58.48,20.9,7.0,55.4,49.5,55.59,2.89,17.5,1618,35,,,,,,
-        """.trimIndent()
+        val weights = listOf(Weight(
+            timestamp = Date.from(Instant.ofEpochMilli(1704057630000)),
+            weight = 76.15f,
+            bmi = 23.8f,
+            fat = 23.2f,
+            visceralFat = 7,
+            water = 55.4f,
+            muscle = 55.59f,
+            bone = 2.89f,
+            basalMet = 1618f,
+            metabolicAge = 35,
+        ))
 
-        val stubInputStream = IOUtils.toInputStream(csvText, "UTF-8")
+        every { Formatter.dateTimeForFilename(any()).format(any()) } returns "20240101_000000"
+        every { FitWriter.weights(any(), any()) } returns Unit
+        coEvery { repo.uploadFile(any()) } returns Result.Success(Unit)
+
+        val res = useCase(weights)
+
+        assertThat(res.isSuccessful).isTrue()
+        verify {
+            Formatter.dateTimeForFilename(any()).format(any())
+            FitWriter.weights(any(), weights)
+        }
+        coVerify { repo.uploadFile(any()) }
+        confirmVerified(repo, Formatter, FitWriter)
+    }
+
+    @Test
+    fun `Upload empty list`() = runTest {
+        every { Formatter.dateTimeForFilename(any()).format(any()) } returns "20240101_000000"
+        every { FitWriter.weights(any(), any()) } returns Unit
+        coEvery { repo.uploadFile(any()) } returns Result.Success(Unit)
+
+        val res = useCase(emptyList<Weight>())
+
+        assertThat(res.isSuccessful).isTrue()
+        verify {
+            Formatter.dateTimeForFilename(any()).format(any())
+            FitWriter.weights(any(), emptyList())
+        }
+        coVerify { repo.uploadFile(any()) }
+        confirmVerified(repo, Formatter, FitWriter)
+    }
+
+    @Test
+    fun `Failed upload`() = runTest {
+        every { Formatter.dateTimeForFilename(any()).format(any()) } returns "20240101_000000"
+        every { FitWriter.weights(any(), any()) } returns Unit
+        coEvery { repo.uploadFile(any()) } returns Result.Failure("Failed to upload file")
 
         val weights = listOf(Weight(
             timestamp = Date.from(Instant.ofEpochMilli(1704057630000)),
@@ -74,68 +116,16 @@ class SyncWeightTest {
             metabolicAge = 35,
         ))
 
-        every { RenphoReader.read(any()) } returns weights
-        every { Formatter.dateTimeForFilename(any()).format(any()) } returns "20240101_000000"
-        every { FitWriter.weights(any(), any()) } returns Unit
-        coEvery { repo.uploadFile(any()) } returns Result.Success(Unit)
-
-        val res = useCase(stubInputStream)
-
-        assertThat(res.isSuccessful).isTrue()
-        verify {
-            RenphoReader.read(stubInputStream)
-            Formatter.dateTimeForFilename(any()).format(any())
-            FitWriter.weights(any(), weights)
-        }
-        coVerify { repo.uploadFile(any()) }
-        confirmVerified(repo, RenphoReader, Formatter, FitWriter)
-    }
-
-    @Test
-    fun `Upload empty file`() = runTest {
-        val csvText = ""
-
-        val stubInputStream = IOUtils.toInputStream(csvText, "UTF-8")
-
-        every { RenphoReader.read(any()) } returns emptyList()
-        every { Formatter.dateTimeForFilename(any()).format(any()) } returns "20240101_000000"
-        every { FitWriter.weights(any(), any()) } returns Unit
-        coEvery { repo.uploadFile(any()) } returns Result.Success(Unit)
-
-        val res = useCase(stubInputStream)
-
-        assertThat(res.isSuccessful).isTrue()
-        verify {
-            RenphoReader.read(stubInputStream)
-            Formatter.dateTimeForFilename(any()).format(any())
-            FitWriter.weights(any(), emptyList())
-        }
-        coVerify { repo.uploadFile(any()) }
-        confirmVerified(repo, RenphoReader, Formatter, FitWriter)
-    }
-
-    @Test
-    fun `Failed upload`() = runTest {
-        val csvText = ""
-
-        val stubInputStream = IOUtils.toInputStream(csvText, "UTF-8")
-
-        every { RenphoReader.read(any()) } returns emptyList()
-        every { Formatter.dateTimeForFilename(any()).format(any()) } returns "20240101_000000"
-        every { FitWriter.weights(any(), any()) } returns Unit
-        coEvery { repo.uploadFile(any()) } returns Result.Failure("Failed to upload file")
-
-        val res = useCase(stubInputStream)
+        val res = useCase(weights)
 
         assertThat(res.isSuccessful).isFalse()
         res as Result.Failure
         assertThat(res.reason).isEqualTo("Failed to upload file")
         verify {
-            RenphoReader.read(stubInputStream)
             Formatter.dateTimeForFilename(any()).format(any())
-            FitWriter.weights(any(), emptyList())
+            FitWriter.weights(any(), weights)
         }
         coVerify { repo.uploadFile(any()) }
-        confirmVerified(repo, RenphoReader, Formatter, FitWriter)
+        confirmVerified(repo, Formatter, FitWriter)
     }
 }
