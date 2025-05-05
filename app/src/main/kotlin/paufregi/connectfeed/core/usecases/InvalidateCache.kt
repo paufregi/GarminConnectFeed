7c7c4084eb1ba84e4cbfa761a41a2ad2839a72af
Paul Ellis <paul@movio.co>
2025-05-06 08:50:01 +1200

    Drop invalidate cache use case

--- app/src/main/kotlin/paufregi/connectfeed/core/usecases/InvalidateCache.kt
+++
@@ -1,8 +0,0 @@
-package paufregi.connectfeed.core.usecases
-
-import paufregi.connectfeed.data.repository.GarminRepository
-import javax.inject.Inject
-
-class InvalidateCache @Inject constructor(private val repository: GarminRepository) {
-    operator fun invoke(): Unit = repository.invalidateCaches()
-}
