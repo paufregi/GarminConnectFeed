8a95492b71ccd8ddd762f1271035c319219e4dfc
Paul Ellis <paul@movio.co>
2025-05-09 20:51:19 +1200

    Use json encoded objects in StravaStore

--- app/src/main/kotlin/paufregi/connectfeed/data/datastore/StravaStore.kt
+++ app/src/main/kotlin/paufregi/connectfeed/data/datastore/StravaStore.kt
@@ -5,36 +5,27 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.byteArrayPreferencesKey
 import androidx.datastore.preferences.core.edit
 import kotlinx.coroutines.flow.map
+import kotlinx.serialization.json.Json
 import paufregi.connectfeed.data.api.strava.models.AuthToken
 import paufregi.connectfeed.data.utils.Crypto
 
 class StravaStore(val dataStore: DataStore<Preferences>) {
 
     companion object {
-        private val ACCESS_TOKEN = byteArrayPreferencesKey("accessToken")
-        private val REFRESH_TOKEN = byteArrayPreferencesKey("refreshToken")
-        private val EXPIRE_AT = byteArrayPreferencesKey("expire_at")
+        private val AUTH_TOKEN = byteArrayPreferencesKey("authToken")
     }
 
     fun getToken() = dataStore.data.map {
-        it[ACCESS_TOKEN]?.let { accessToken ->
-            it[REFRESH_TOKEN]?.let { refreshToken ->
-                it[EXPIRE_AT]?.let { expireAt ->
-                    AuthToken(
-                        accessToken = Crypto.decrypt(accessToken),
-                        refreshToken = Crypto.decrypt(refreshToken),
-                        expiresAt = Crypto.decrypt(expireAt).toLong()
-                    )
-                }
+        it[AUTH_TOKEN]?.let { data ->
+            Crypto.decrypt(data).let {
+                Json.decodeFromString<AuthToken>(it)
             }
         }
     }
 
     suspend fun saveToken(authToken: AuthToken) {
         dataStore.edit {
-            it[ACCESS_TOKEN] = Crypto.encrypt(authToken.accessToken)
-            it[REFRESH_TOKEN] = Crypto.encrypt(authToken.refreshToken)
-            it[EXPIRE_AT] = Crypto.encrypt(authToken.expiresAt.toString())
+            it[AUTH_TOKEN] = Crypto.encrypt(Json.encodeToString(authToken))
         }
     }
 
