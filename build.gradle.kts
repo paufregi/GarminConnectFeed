fb7d0694a7e08d4e8d7fc27d9e2bf9fb459de61b
Paul <paulfrancis.ellis@gmail.com>
2025-05-11 08:45:25 +1200

    Gradle versions plugin (#137)
    * Add gradle-versions-plugin plugin
    
    * Reject all non stable versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -7,6 +9,7 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.dependency.analysis) apply false
+    alias(libs.plugins.dependency.versions)
 }
 
 subprojects {
@@ -17,4 +20,17 @@ subprojects {
 }
 repositories {
     google()
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
 }
