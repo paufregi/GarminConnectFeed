3f6411bfcdfbedebca7b13cc9549d63dd2cbfadc
Paul Ellis <paul@movio.co>
2025-05-11 01:17:58 +1200

    Add gradle-versions-plugin plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.dependency.analysis) apply false
+    alias(libs.plugins.dependency.versions)
 }
 
 subprojects {
