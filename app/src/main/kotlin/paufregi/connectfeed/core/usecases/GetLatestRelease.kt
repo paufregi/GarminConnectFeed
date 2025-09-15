a3c1ef8fe42674c1fc0a191af9dd2f6da671eb9f
Paul Ellis <paul@movio.co>
2025-09-15 15:00:40 +1200

    Add use cases to retrieve current version and latests release

---
+++ app/src/main/kotlin/paufregi/connectfeed/core/usecases/GetLatestRelease.kt
@@ -0,0 +1,9 @@
+package paufregi.connectfeed.core.usecases
+
+import paufregi.connectfeed.core.models.Release
+import paufregi.connectfeed.data.repository.GithubRepository
+import javax.inject.Inject
+
+class GetLatestRelease @Inject constructor(val repo: GithubRepository) {
+    suspend operator fun invoke(): Result<Release?> = repo.getLatestRelease()
+}
