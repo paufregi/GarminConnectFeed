d538550fc1a682921958181ef16df290be7564e7
Paul Ellis <paul@movio.co>
2025-09-18 17:28:51 +1200

    WIP - Info presentation section

---
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/info/InfoState.kt
@@ -0,0 +1,11 @@
+package paufregi.connectfeed.presentation.info
+
+import paufregi.connectfeed.core.models.Release
+import paufregi.connectfeed.core.models.Version
+import paufregi.connectfeed.presentation.ui.models.ProcessState
+
+data class InfoState(
+    val process: ProcessState = ProcessState.Processing,
+    val currentVersion: Version? = null,
+    val latestRelease: Release? = null,
+)
