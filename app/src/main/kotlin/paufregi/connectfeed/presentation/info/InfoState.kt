9729ba747e455a5b75cfed5f590e0c6fff01b148
Paul Ellis <paul@movio.co>
2025-09-19 10:45:04 +1200

    Refactor InfoState to infer hasUpdate

--- app/src/main/kotlin/paufregi/connectfeed/presentation/info/InfoState.kt
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/info/InfoState.kt
@@ -5,7 +5,10 @@ import paufregi.connectfeed.core.models.Version
 import paufregi.connectfeed.presentation.ui.models.ProcessState
 
 data class InfoState(
-    val process: ProcessState = ProcessState.Processing,
+    val process: ProcessState = ProcessState.Idle,
     val currentVersion: Version? = null,
     val latestRelease: Release? = null,
-)
+) {
+    val hasUpdate: Boolean
+        get() = latestRelease?.version?.let { currentVersion?.isLowerThan(it) } ?: false
+}
