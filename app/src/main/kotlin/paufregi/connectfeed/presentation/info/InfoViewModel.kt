59d6d3b61e685db72fcbe5537d7699d7f992a1df
Paul Ellis <paul@movio.co>
2025-09-19 10:46:00 +1200

    Refactor InfoViewModel - still WIP

--- app/src/main/kotlin/paufregi/connectfeed/presentation/info/InfoViewModel.kt
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/info/InfoViewModel.kt
@@ -4,9 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import paufregi.connectfeed.core.models.Version
@@ -21,29 +19,23 @@ class InfoViewModel @Inject constructor(
     val getLatestRelease: GetLatestRelease,
 ) : ViewModel() {
 
-    private val _state = MutableStateFlow(InfoState())
+    private val _state = MutableStateFlow(InfoState(currentVersion = Version.parse(version)))
 
-    val state = _state
-        .onStart { load() }
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), InfoState())
-
-
-    private fun load() = viewModelScope.launch {
-        _state.update { it.copy(
-            process = ProcessState.Processing,
-            currentVersion = Version.parse(version)
-        )}
-
-        getLatestRelease()
-            .onSuccess { data ->
-                _state.update { it.copy(latestRelease = data, process = ProcessState.Success("Ok")) } }
-            .onFailure { _state.update { it.copy(process = ProcessState.Failure("Couldn't load latest release")) } }
-    }
+    val state = _state.asStateFlow()
 
     fun onAction(action: InfoAction) {
         when(action) {
+            InfoAction.CheckUpdate -> checkUpdatesAction()
             InfoAction.Update -> {}
         }
+    }
+
+    private fun checkUpdatesAction() = viewModelScope.launch {
+        _state.update { it.copy(process = ProcessState.Processing)}
+
+        getLatestRelease()
+            .onSuccess { data -> _state.update { it.copy(latestRelease = data, process = ProcessState.Success("Ok")) } }
+            .onFailure { _state.update { it.copy(process = ProcessState.Failure("Couldn't load latest release")) } }
     }
 
 }
