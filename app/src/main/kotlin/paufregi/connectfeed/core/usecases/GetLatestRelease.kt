ba9be239ef1d83ac4062224b088e3218a1bb16ab
Paul Ellis <paul@movio.co>
2025-09-18 17:27:19 +1200

    Clean up GetLatestRelease

--- app/src/main/kotlin/paufregi/connectfeed/core/usecases/GetLatestRelease.kt
+++ app/src/main/kotlin/paufregi/connectfeed/core/usecases/GetLatestRelease.kt
@@ -5,5 +5,5 @@ import paufregi.connectfeed.data.repository.GithubRepository
 import javax.inject.Inject
 
 class GetLatestRelease @Inject constructor(val repo: GithubRepository) {
-    suspend operator fun invoke(): Result<Release?> = repo.getLatestRelease()
+    suspend operator fun invoke(): Result<Release> = repo.getLatestRelease()
 }
