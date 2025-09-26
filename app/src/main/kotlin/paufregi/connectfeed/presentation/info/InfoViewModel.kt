2e291ee7e193e8f9032ffcada3ea25cd2ecd982b
Paul Ellis <paul@movio.co>
2025-09-26 21:50:36 +1200

    Wire the downloader into InfoViewModel

--- app/src/main/kotlin/paufregi/connectfeed/presentation/info/InfoViewModel.kt
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/info/InfoViewModel.kt
@@ -5,21 +5,19 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import paufregi.connectfeed.core.models.Version
 import paufregi.connectfeed.core.usecases.GetLatestRelease
-import paufregi.connectfeed.data.utils.Updater
 import paufregi.connectfeed.presentation.ui.models.ProcessState
+import paufregi.connectfeed.system.Downloader
 import javax.inject.Inject
 import javax.inject.Named
 
 @HiltViewModel
 class InfoViewModel @Inject constructor(
     @param:Named("currentVersion") val version: String,
-    @param:Named("updater") val updater: Updater,
+    @param:Named("downloader") val downloader: Downloader,
     val getLatestRelease: GetLatestRelease,
 ) : ViewModel() {
 
@@ -39,21 +37,13 @@ class InfoViewModel @Inject constructor(
 
         getLatestRelease()
             .onSuccess { data -> _state.update { it.copy(latestRelease = data, process = ProcessState.Idle) } }
-            .onFailure { _state.update { it.copy(latestRelease = null, process = ProcessState.Failure("Couldn't load latest release")) } }
+            .onFailure { _state.update { it.copy(latestRelease = null, process = ProcessState.Failure("Couldn't get latest release")) } }
     }
 
     private fun update() = viewModelScope.launch {
-        state.value.latestRelease?.let {
-            updater.downloadApk(it)
-                .onSuccess { it.collect { download ->
-                        when {
-                            download.progress == 100L && download.uri != null -> updater.installApk(download.uri)
-                            else -> _state.update { it.copy(downloadProgress = download.progress) }
-                        }
-                    }
-                }
-                .onFailure { _state.update { it.copy(process = ProcessState.Failure("Couldn't start download")) } }
+        state.value.latestRelease?.let { release ->
+            _state.update { it.copy(process = ProcessState.Processing) }
+            downloader.downloadApk(release)
         }
     }
-
 }
