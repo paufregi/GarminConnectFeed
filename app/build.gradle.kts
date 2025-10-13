e22a3d21af0851d01ccbdd3cfc24e5771bb8e464
Paul Ellis <paul@movio.co>
2025-10-13 16:18:35 +1300

    v2.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "paufregi.connectfeed"
         minSdk = 33
         targetSdk = 35
-        versionCode = 23
-        versionName = "2.2.2"
+        versionCode = 24
+        versionName = "2.2.3"
 
         testInstrumentationRunner = "paufregi.connectfeed.TestRunner"
         vectorDrawables {
