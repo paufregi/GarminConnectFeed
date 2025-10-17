cf162dfa1876ce1cf8565f27e6e53bf41c9e4346
Paul Ellis <paul@movio.co>
2025-10-18 00:05:09 +1300

    Remove CheckUpdate action

--- app/src/main/kotlin/paufregi/connectfeed/presentation/settings/SettingsAction.kt
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/settings/SettingsAction.kt
@@ -3,8 +3,7 @@ package paufregi.connectfeed.presentation.settings
 sealed interface SettingsAction {
     data object RefreshUser : SettingsAction
     data object SignOut : SettingsAction
-    data object CheckUpdate : SettingsAction
-    data object Update : SettingsAction
     data object StravaDisconnect : SettingsAction
+    data object Update : SettingsAction
     data object Reset : SettingsAction
 }
