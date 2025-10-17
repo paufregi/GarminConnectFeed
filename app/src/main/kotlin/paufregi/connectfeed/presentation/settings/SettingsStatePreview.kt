7b7c26872cabd0776aee12f986882839d52dc7fa
Paul Ellis <paul@movio.co>
2025-10-18 00:06:38 +1300

    State preview

--- app/src/main/kotlin/paufregi/connectfeed/presentation/settings/SettingsStatePreview.kt
+++ app/src/main/kotlin/paufregi/connectfeed/presentation/settings/SettingsStatePreview.kt
@@ -1,14 +1,17 @@
 package paufregi.connectfeed.presentation.settings
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import paufregi.connectfeed.presentation.ui.models.ProcessState
+import paufregi.connectfeed.core.models.Release
+import paufregi.connectfeed.core.models.User
+import paufregi.connectfeed.core.models.Version
 
 class SettingsStatePreview : PreviewParameterProvider<SettingsState> {
+    val user = User(1, "John Doe", "https://example.com")
+    val version = Version(1, 2, 3)
+
     override val values = sequenceOf(
-        SettingsState(process = ProcessState.Processing),
-        SettingsState(process = ProcessState.Success("Profile saved")),
-        SettingsState(process = ProcessState.Failure("Error saving profile")),
-        SettingsState(process = ProcessState.Idle, hasStrava = false),
-        SettingsState(process = ProcessState.Idle, hasStrava = true),
+        SettingsState(hasStrava = false, user = user, currentVersion = version, latestRelease = Release(version, "https://example.com")),
+        SettingsState(hasStrava = true, user = user, currentVersion = version, latestRelease = Release(Version(2,0,0), "https://example.com")),
+        SettingsState(hasStrava = true, user = user, currentVersion = version, latestRelease = Release(Version(2,0,0), "https://example.com"), updating = true),
     )
 }
