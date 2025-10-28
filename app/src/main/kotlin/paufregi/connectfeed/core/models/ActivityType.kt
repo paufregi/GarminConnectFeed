396aa4c83392239b9ae0d481d3d3f1094ee6cb2c
Paul Ellis <paul@movio.co>
2025-10-28 20:14:48 +1300

    Refactor ActivityTypes

--- app/src/main/kotlin/paufregi/connectfeed/core/models/ActivityType.kt
+++ app/src/main/kotlin/paufregi/connectfeed/core/models/ActivityType.kt
@@ -1,28 +1,88 @@
 package paufregi.connectfeed.core.models
 
-sealed class ActivityType(val name: String, val order: Int, val allowCourseInProfile: Boolean) {
-    data object Any : ActivityType("Any", 1, false)
-    data object Running : ActivityType("Running", 2, true)
-    data object TrailRunning : ActivityType("Trail Running", 3, true)
-    data object TreadmillRunning : ActivityType("Treadmill Running", 4, false)
-    data object Cycling : ActivityType("Cycling", 5, true)
-    data object IndoorCycling : ActivityType("Indoor Cycling", 6, false)
-    data object EBiking : ActivityType("eBiking", 7, true)
-    data object Swimming : ActivityType("Swimming", 8, false)
-    data object OpenWaterSwimming : ActivityType("Open Water Swimming", 9, false)
-    data object Strength : ActivityType("Strength", 10, false)
-    data object Fitness : ActivityType("Fitness", 11, false)
-    data object Walking : ActivityType("Walking", 12, true)
-    data object Hiking : ActivityType("Hiking", 13, true)
-    data object Rugby : ActivityType("Rugby", 14, false)
-    data object Soccer : ActivityType("Soccer", 15, false)
-    data object JumpRope : ActivityType("Jump Rope", 16, false)
-    data object Breathwork : ActivityType("Breathwork", 17, false)
-    data object Meditation : ActivityType("Meditation", 18, false)
-    data object Yoga : ActivityType("Yoga", 19, false)
-    data object Other : ActivityType("Other", 99, false)
-    data object Unknown : ActivityType("Unknown", 100, false)
+sealed interface ActivityType {
+    // GARMIN
 
-    fun match(activityType: ActivityType?): Boolean =
-        this == Any || activityType == null || this == activityType
+    // Running
+    data object Running: ActivityType // 1
+    data object TrackRunning: ActivityType // 8
+    data object TrailRunning: ActivityType // 6
+    data object TreadmillRunning: ActivityType // 18
+    data object UltraRun: ActivityType // 181
+
+    // Cycling
+    data object Cycling: ActivityType // 2
+    data object DownhillBiking: ActivityType // 20
+    data object EBiking: ActivityType // 176
+    data object EBikingMountain: ActivityType // 175
+    data object GravelCycling: ActivityType // 143
+    data object MountainBiking: ActivityType // 5
+    data object RoadBiking: ActivityType // 10
+    data object VirtualRide: ActivityType // 153
+
+    // Fitness
+    data object HIIT: ActivityType // 180
+    data object Breathwork: ActivityType // 164
+    data object Cardio: ActivityType // 11
+    data object JumpRope: ActivityType // 254
+    data object StrengthTraining: ActivityType // 13
+    data object Yoga: ActivityType // 163
+
+    // Swimming
+    data object Swimming: ActivityType
+    data object PoolSwimming: ActivityType // 27
+    data object OpenWaterSwimming: ActivityType // 28
+
+    // Other
+    data object Walking: ActivityType // 9
+    data object Hiking: ActivityType // 3
+
+    // Other
+    data object Snowboarding: ActivityType // 252
+    data object Kayaking: ActivityType // 231
+    data object StandUpPaddling: ActivityType // 239
+    data object Surfing: ActivityType // 240
+    data object Windsurf: ActivityType // 242
+
+    // Other
+    data object Football: ActivityType // 215
+    data object MultiSport: ActivityType // 89
+    data object Other: ActivityType // 4
+
+    // STRAVA
+    // Running
+    data object StravaRunning: ActivityType // Run
+    data object StravaTrailRun: ActivityType // TrailRun
+
+    // Cycling
+    data object StravaRide: ActivityType // Ride
+    data object StravaMountainBikeRide: ActivityType // MountainBikeRide
+    data object StravaGravelRide: ActivityType // GravelRide
+    data object StravaEBikeRide: ActivityType // EBikeRide
+    data object StravaEMountainBikeRide: ActivityType // EMountainBikeRide
+    data object StravaVirtualRide: ActivityType // VirtualRide
+
+    // Fitness
+    data object StravaHIIT: ActivityType // HighIntensityIntervalTraining
+    data object StravaWorkout: ActivityType // Workout
+    data object StravaWeightTraining: ActivityType // WeightTraining
+    data object StravaYoga: ActivityType // Yoga
+
+    // Swimming
+    data object StravaSwim: ActivityType // Swim
+
+    // Other
+    data object StravaWalk: ActivityType // Walk
+    data object StravaHike: ActivityType // Hike
+
+    // Other
+    data object StravaSnowboard: ActivityType // Snowboard
+    data object StravaKayaking: ActivityType // Kayaking
+    data object StravaStandUpPaddling: ActivityType // StandUpPaddling
+    data object StravaSurfing: ActivityType // Surfing
+    data object StravaWindsurf: ActivityType // Windsurf
+
+    // Other
+    data object StravaFootball: ActivityType // Soccer
 }
+
