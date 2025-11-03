d9cf1c96df03cf49355f5193b52a3629af363330
Paul Ellis <paul@movio.co>
2025-11-04 09:36:30 +1300

    Add profile type and refactor ActivityType

--- app/src/main/kotlin/paufregi/connectfeed/core/models/ActivityType.kt
+++ app/src/main/kotlin/paufregi/connectfeed/core/models/ActivityType.kt
@@ -1,28 +1,83 @@
 package paufregi.connectfeed.core.models
 
-sealed class ActivityType(val name: String, val order: Int, val allowCourseInProfile: Boolean) {
-    data object Any : ActivityType("Any", 1, false)
-    data object Running : ActivityType("Running", 2, true)
-    data object TrailRunning : ActivityType("Trail Running", 3, true)
-    data object TreadmillRunning : ActivityType("Treadmill Running", 4, false)
-    data object Cycling : ActivityType("Cycling", 5, true)
-    data object IndoorCycling : ActivityType("Indoor Cycling", 6, false)
-    data object EBiking : ActivityType("eBiking", 7, true)
-    data object Swimming : ActivityType("Swimming", 8, false)
-    data object OpenWaterSwimming : ActivityType("Open Water Swimming", 9, false)
-    data object Strength : ActivityType("Strength", 10, false)
-    data object Fitness : ActivityType("Fitness", 11, false)
-    data object Walking : ActivityType("Walking", 12, true)
-    data object Hiking : ActivityType("Hiking", 13, true)
-    data object Rugby : ActivityType("Rugby", 14, false)
-    data object Soccer : ActivityType("Soccer", 15, false)
-    data object JumpRope : ActivityType("Jump Rope", 16, false)
-    data object Breathwork : ActivityType("Breathwork", 17, false)
-    data object Meditation : ActivityType("Meditation", 18, false)
-    data object Yoga : ActivityType("Yoga", 19, false)
-    data object Other : ActivityType("Other", 99, false)
-    data object Unknown : ActivityType("Unknown", 100, false)
+sealed class ActivityType(val profileType: ProfileType) {
+    // GARMIN
+    // Running
+    data object Running: ActivityType(ProfileType.Running) // 1
+    data object TrackRunning: ActivityType(ProfileType.Running) // 8
+    data object TrailRunning: ActivityType(ProfileType.Running) // 6
+    data object TreadmillRunning: ActivityType(ProfileType.Running) // 18
+    data object UltraRun: ActivityType(ProfileType.Running) // 181
 
-    fun match(activityType: ActivityType?): Boolean =
-        this == Any || activityType == null || this == activityType
+    // Cycling
+    data object Cycling: ActivityType(ProfileType.Cycling) // 2
+    data object DownhillBiking: ActivityType(ProfileType.Cycling) // 20
+    data object EBiking: ActivityType(ProfileType.Cycling) // 176
+    data object EBikingMountain: ActivityType(ProfileType.Cycling) // 175
+    data object GravelCycling: ActivityType(ProfileType.Cycling) // 143
+    data object MountainBiking: ActivityType(ProfileType.Cycling) // 5
+    data object RoadBiking: ActivityType(ProfileType.Cycling) // 10
+    data object VirtualRide: ActivityType(ProfileType.Cycling) // 153
+
+    // Fitness
+    data object HIIT: ActivityType(ProfileType.Fitness) // 180
+    data object Breathwork: ActivityType(ProfileType.Fitness) // 164
+    data object Cardio: ActivityType(ProfileType.Fitness) // 11
+    data object JumpRope: ActivityType(ProfileType.Fitness) // 254
+    data object StrengthTraining: ActivityType(ProfileType.Fitness) // 13
+    data object Yoga: ActivityType(ProfileType.Fitness) // 163
+
+    // Swimming
+    data object Swimming: ActivityType(ProfileType.Swimming)
+    data object PoolSwimming: ActivityType(ProfileType.Swimming) // 27
+    data object OpenWaterSwimming: ActivityType(ProfileType.Swimming) // 28
+
+    // Other
+    data object Walking: ActivityType(ProfileType.Other) // 9
+    data object Hiking: ActivityType(ProfileType.Other) // 3
+
+    // Other
+    data object Snowboarding: ActivityType(ProfileType.Other) // 252
+    data object Kayaking: ActivityType(ProfileType.Other) // 231
+    data object StandUpPaddling: ActivityType(ProfileType.Other) // 239
+    data object Surfing: ActivityType(ProfileType.Other) // 240
+    data object Windsurf: ActivityType(ProfileType.Other) // 242
+
+    // STRAVA
+    // Running
+    data object StravaRunning: ActivityType(ProfileType.Running) // Run
+    data object StravaTrailRun: ActivityType(ProfileType.Running) // TrailRun
+
+    // Cycling
+    data object StravaRide: ActivityType(ProfileType.Cycling) // Ride
+    data object StravaMountainBikeRide: ActivityType(ProfileType.Cycling) // MountainBikeRide
+    data object StravaGravelRide: ActivityType(ProfileType.Cycling) // GravelRide
+    data object StravaEBikeRide: ActivityType(ProfileType.Cycling) // EBikeRide
+    data object StravaEMountainBikeRide: ActivityType(ProfileType.Cycling) // EMountainBikeRide
+    data object StravaVirtualRide: ActivityType(ProfileType.Cycling) // VirtualRide
+
+    // Fitness
+    data object StravaHIIT: ActivityType(ProfileType.Fitness) // HighIntensityIntervalTraining
+    data object StravaWorkout: ActivityType(ProfileType.Fitness) // Workout
+    data object StravaWeightTraining: ActivityType(ProfileType.Fitness) // WeightTraining
+    data object StravaYoga: ActivityType(ProfileType.Fitness) // Yoga
+
+    // Swimming
+    data object StravaSwim: ActivityType(ProfileType.Swimming) // Swim
+
+    // Other
+    data object StravaWalk: ActivityType(ProfileType.Other) // Walk
+    data object StravaHike: ActivityType(ProfileType.Other) // Hike
+
+    // Other
+    data object StravaSnowboard: ActivityType(ProfileType.Other) // Snowboard
+    data object StravaKayaking: ActivityType(ProfileType.Other) // Kayaking
+    data object StravaStandUpPaddling: ActivityType(ProfileType.Other) // StandUpPaddling
+    data object StravaSurfing: ActivityType(ProfileType.Other) // Surfing
+    data object StravaWindsurf: ActivityType(ProfileType.Other) // Windsurf
+
+    // Other
+    data object StravaFootball: ActivityType(ProfileType.Other) // Soccer
+
+    data object Unknown : ActivityType(ProfileType.Other)
 }
---
+++ app/src/main/kotlin/paufregi/connectfeed/core/models/ProfileType.kt
@@ -0,0 +1,11 @@
+package paufregi.connectfeed.core.models
+
+sealed class ProfileType(val order: Int, val allowCourse: Boolean) {
+    data object Any : ProfileType(1,false)
+    data object Running : ProfileType(2, true)
+    data object Cycling : ProfileType(3, true)
+    data object Swimming : ProfileType(4, false)
+    data object Strength : ProfileType(5, false)
+    data object Fitness : ProfileType(6, false)
+    data object Other : ProfileType(7, false)
+}
