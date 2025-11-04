6448a199db078ed093f60592f220290c7170b5ef
Paul Ellis <paul@movio.co>
2025-11-04 16:22:38 +1300

    Update Strava API to match core models

--- app/src/main/kotlin/paufregi/connectfeed/data/api/strava/converters/SportTypeConverter.kt
+++ app/src/main/kotlin/paufregi/connectfeed/data/api/strava/converters/SportTypeConverter.kt
@@ -4,56 +4,38 @@ import paufregi.connectfeed.core.models.ActivityType
 
 object SportTypeConverter {
     fun toActivityType(type: String): ActivityType = when (type) {
-        "AlpineSki" -> ActivityType.Unknown
-        "BackcountrySki" -> ActivityType.Unknown
-        "Badminton" -> ActivityType.Unknown
-        "Canoeing" -> ActivityType.Unknown
-        "Crossfit" -> ActivityType.Fitness
-        "EBikeRide" -> ActivityType.EBiking
-        "Elliptical" -> ActivityType.Unknown
-        "EMountainBikeRide" -> ActivityType.EBiking
-        "Golf" -> ActivityType.Unknown
-        "GravelRide" -> ActivityType.Cycling
-        "Handcycle" -> ActivityType.Cycling
-        "HighIntensityIntervalTraining" -> ActivityType.Fitness
-        "Hike" -> ActivityType.Hiking
-        "IceSkate" -> ActivityType.Unknown
-        "InlineSkate" -> ActivityType.Unknown
-        "Kayaking" -> ActivityType.Unknown
-        "Kitesurf" -> ActivityType.Unknown
-        "MountainBikeRide" -> ActivityType.Cycling
-        "NordicSki" -> ActivityType.Unknown
-        "Pickleball" -> ActivityType.Unknown
-        "Pilates" -> ActivityType.Other
-        "Racquetball" -> ActivityType.Unknown
-        "Ride" -> ActivityType.Cycling
-        "RockClimbing" -> ActivityType.Unknown
-        "RollerSki" -> ActivityType.Unknown
-        "Rowing" -> ActivityType.Unknown
-        "Run" -> ActivityType.Running
-        "Sail" -> ActivityType.Unknown
-        "Skateboard" -> ActivityType.Unknown
-        "Snowboard" -> ActivityType.Unknown
-        "Snowshoe" -> ActivityType.Unknown
-        "Soccer" -> ActivityType.Soccer
-        "Squash" -> ActivityType.Unknown
-        "StairStepper" -> ActivityType.Unknown
-        "StandUpPaddling" -> ActivityType.Unknown
-        "Surfing" -> ActivityType.Unknown
-        "Swim" -> ActivityType.Swimming
-        "TableTennis" -> ActivityType.Unknown
-        "Tennis" -> ActivityType.Unknown
-        "TrailRun" -> ActivityType.TrailRunning
-        "Velomobile" -> ActivityType.Cycling
-        "VirtualRide" -> ActivityType.Cycling
-        "VirtualRow" -> ActivityType.Unknown
-        "VirtualRun" -> ActivityType.Running
-        "Walk" -> ActivityType.Walking
-        "WeightTraining" -> ActivityType.Strength
-        "Wheelchair" -> ActivityType.Unknown
-        "Windsurf" -> ActivityType.Unknown
-        "Workout" -> ActivityType.Fitness
-        "Yoga" -> ActivityType.Yoga
+        // Running
+        "Run" -> ActivityType.StravaRunning
+        "TrailRun" -> ActivityType.StravaTrailRun
+        "VirtualRun" -> ActivityType.StravaRunning
+
+        // Cycling
+        "Ride" -> ActivityType.StravaRide
+        "MountainBikeRide" -> ActivityType.StravaMountainBikeRide
+        "GravelRide" -> ActivityType.StravaGravelRide
+        "EBikeRide" -> ActivityType.StravaEBikeRide
+        "EMountainBikeRide" -> ActivityType.StravaEMountainBikeRide
+        "VirtualRide" -> ActivityType.StravaVirtualRide
+
+        // Fitness
+        "HighIntensityIntervalTraining" -> ActivityType.StravaHIIT
+        "Workout" -> ActivityType.StravaWorkout
+        "WeightTraining" -> ActivityType.StravaWeightTraining
+        "Yoga" -> ActivityType.StravaYoga
+
+        // Swimming
+        "Swim" -> ActivityType.StravaSwim
+
+        // Other
+        "Walk" -> ActivityType.StravaWalk
+        "Hike" -> ActivityType.StravaHike
+        "Snowboard" -> ActivityType.StravaSnowboard
+        "Kayaking" -> ActivityType.StravaKayaking
+        "StandUpPaddling" -> ActivityType.StravaStandUpPaddling
+        "Surfing" -> ActivityType.StravaSurfing
+        "Windsurf" -> ActivityType.StravaWindsurf
+        "Soccer" -> ActivityType.StravaFootball
+
         else -> ActivityType.Unknown
     }
 }
