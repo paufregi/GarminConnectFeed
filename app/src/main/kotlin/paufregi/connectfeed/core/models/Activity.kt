15ae202ad0137dfba2adfd44c237820af13f5357
Paul Ellis <paul@movio.co>
2025-11-06 23:24:30 +1300

    Update activity match function to use profile

--- app/src/main/kotlin/paufregi/connectfeed/core/models/Activity.kt
+++ app/src/main/kotlin/paufregi/connectfeed/core/models/Activity.kt
@@ -13,5 +13,8 @@ data class Activity(
     val date: Instant? = null
 ) {
     fun match(other: Activity): Boolean =
-        this.type == other.type && (this.date != null && other.date != null && Duration.between(this.date, other.date).abs() <= Duration.ofMinutes(1))
+        this.type.profileType.compatible(other.type) &&
+                (this.date != null && other.date != null && Duration.between(this.date, other.date).abs() <= Duration.ofMinutes(1))
+
+    
 }
