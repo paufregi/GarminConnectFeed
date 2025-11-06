7c0706180390f46a05aa255eb5aab4a9d71b9e82
Paul Ellis <paul@movio.co>
2025-11-06 23:32:49 +1300

    Add default values to DB table

--- app/src/main/kotlin/paufregi/connectfeed/data/database/entities/ProfileEntity.kt
+++ app/src/main/kotlin/paufregi/connectfeed/data/database/entities/ProfileEntity.kt
@@ -1,5 +1,6 @@
 package paufregi.connectfeed.data.database.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -14,13 +15,18 @@ data class ProfileEntity(
 
     val userId: Long,
     val name: String,
+    @ColumnInfo(defaultValue = "Any")
     val type: ProfileType = ProfileType.Any,
     val eventType: EventType? = null,
     @Embedded(prefix = "course_")
     val course: Course? = null,
     val water: Int? = null,
+    @ColumnInfo(defaultValue = "true")
     val rename: Boolean = true,
+    @ColumnInfo(defaultValue = "false")
     val customWater: Boolean = false,
+    @ColumnInfo(defaultValue = "false")
     val feelAndEffort: Boolean = false,
+    @ColumnInfo(defaultValue = "false")
     val trainingEffect: Boolean = false,
 )
