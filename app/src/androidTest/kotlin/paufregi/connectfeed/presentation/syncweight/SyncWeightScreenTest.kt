623a57f81a53385d8d8649b16c07318e1464c3bc
Paul <paulfrancis.ellis@gmail.com>
2026-01-23 08:23:32 +1300

    Refactor Gradle 9.0 (#313)
    * Remove org.jetbrains.kotlin.android plugin
    
    * Remove not needed flag (android.usesSdkInManifest.disallowed=false)
    
    * Remove not needed flag (android.sdk.defaultTargetSdkToCompileSdkIfUnset=false)
    
    * Remove not needed flag (android.enableR8.fullMode=false)
    
    * Remove not needed flag (android.enableAppCompileTimeRClass=false)
    
    * Remove not needed flag (android.r8.optimizedResourceShrinking=false)
    
    * Remove not needed flag (android.defaults.buildfeatures.resvalues=true)
    
    * Set android.dependency.useConstraints to false
    
    * Speed up build setting parallel and cache flags
    
    * Format

--- app/src/androidTest/kotlin/paufregi/connectfeed/presentation/syncweight/SyncWeightScreenTest.kt
+++ app/src/androidTest/kotlin/paufregi/connectfeed/presentation/syncweight/SyncWeightScreenTest.kt
@@ -1,4 +1,4 @@
-package paufregi.connectfeed.presentation.syncWeight
+package paufregi.connectfeed.presentation.syncweight
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.test.assertIsDisplayed
@@ -6,12 +6,11 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import paufregi.connectfeed.presentation.syncweight.SyncWeightScreen
-import paufregi.connectfeed.presentation.syncweight.SyncWeightState
 import paufregi.connectfeed.presentation.ui.models.ProcessState
 
 @HiltAndroidTest
@@ -19,7 +18,10 @@ import paufregi.connectfeed.presentation.ui.models.ProcessState
 @RunWith(AndroidJUnit4::class)
 class SyncWeightScreenTest {
 
-    @get:Rule
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
     val composeTestRule = createComposeRule()
 
     @Test
