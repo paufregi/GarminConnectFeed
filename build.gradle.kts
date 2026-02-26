405148e19c4b978048f1fa609339e314e82922e1
Paul Ellis <paul@movio.co>
2026-02-27 10:33:44 +1300

    Remove dependency analysis plugin from build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,6 @@ plugins {
     alias(libs.plugins.android.room) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.dependency.analysis) apply false
-    alias(libs.plugins.dependency.versions)
 }
 
 subprojects {
