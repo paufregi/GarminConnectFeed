6530437de13ce971321570484b1940e3384eab83
Paul Ellis <paul@movio.co>
2026-04-30 16:00:23 +1200

    Replace androidx.compose.ui.test.junit4.createComposeRule with androidx.compose.ui.test.junit4.v2.createComposeRule

--- app/src/androidTest/kotlin/paufregi/connectfeed/presentation/settings/SettingsScreenTest.kt
+++ app/src/androidTest/kotlin/paufregi/connectfeed/presentation/settings/SettingsScreenTest.kt
@@ -4,7 +4,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.assertIsNotEnabled
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
