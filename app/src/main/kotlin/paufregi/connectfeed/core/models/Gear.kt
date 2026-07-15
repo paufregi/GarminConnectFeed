cb0114376b23b446aa6f321df492f215d6cefd67
Paul Ellis <paul@movio.co>
2026-07-15 16:34:04 +1200

    Rename Gear field (gearType -> type)

--- app/src/main/kotlin/paufregi/connectfeed/core/models/Gear.kt
+++ app/src/main/kotlin/paufregi/connectfeed/core/models/Gear.kt
@@ -3,6 +3,6 @@ package paufregi.connectfeed.core.models
 data class Gear(
     val id: String,
     val name: String,
-    val gearType: GearType? = null,
+    val type: GearType? = null,
     val distance: Long? = null,
 )
