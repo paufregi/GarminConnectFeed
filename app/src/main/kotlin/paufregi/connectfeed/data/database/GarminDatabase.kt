02b4d31e5030387c24a5dfdf647c04ff42ffc5a1
Paul Ellis <paul@movio.co>
2026-08-07 09:13:46 +1200

    Add Gear entity to DB definition

--- app/src/main/kotlin/paufregi/connectfeed/data/database/GarminDatabase.kt
+++ app/src/main/kotlin/paufregi/connectfeed/data/database/GarminDatabase.kt
@@ -5,14 +5,16 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import paufregi.connectfeed.data.database.coverters.ActivityTypeConverter
 import paufregi.connectfeed.data.database.coverters.EventTypeConverter
+import paufregi.connectfeed.data.database.coverters.GearTypeConverter
+import paufregi.connectfeed.data.database.entities.GearEntity
 import paufregi.connectfeed.data.database.entities.ProfileEntity
 
 @Database(
-    entities = [ProfileEntity::class],
+    entities = [ProfileEntity::class, GearEntity::class],
     version = 1,
     autoMigrations = []
 )
-@TypeConverters(ActivityTypeConverter::class, EventTypeConverter::class)
+@TypeConverters(ActivityTypeConverter::class, EventTypeConverter::class, GearTypeConverter::class)
 abstract class GarminDatabase : RoomDatabase() {
     abstract fun garminDao(): GarminDao
 }
