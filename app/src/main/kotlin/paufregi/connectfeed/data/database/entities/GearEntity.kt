678450d68fcd4f0a688ddc03cc58114f15a9d69f
Paul Ellis <paul@movio.co>
2026-08-07 19:20:18 +1200

    Add StravaGear FK to Gear entity

--- app/src/main/kotlin/paufregi/connectfeed/data/database/entities/GearEntity.kt
+++ app/src/main/kotlin/paufregi/connectfeed/data/database/entities/GearEntity.kt
@@ -1,10 +1,25 @@
 package paufregi.connectfeed.data.database.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import paufregi.connectfeed.core.models.GearType
 
-@Entity(tableName = "gears")
+@Entity(
+    tableName = "gears",
+    foreignKeys = [
+        ForeignKey(
+            entity = StravaGearEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["stravaGearId"],
+            onDelete = ForeignKey.SET_NULL,
+            onUpdate = ForeignKey.CASCADE,
+        )
+    ],
+    indices = [Index("stravaGearId")]
+)
 data class GearEntity(
     @PrimaryKey
     val id: String,
@@ -12,5 +27,8 @@ data class GearEntity(
     val userId: Long,
     val name: String,
     val type: GearType = GearType.Unknown,
-    val distance: Long? = 0
+    val distance: Long? = 0,
+
+    @ColumnInfo(defaultValue = "NULL")
+    val stravaGearId: String? = null,
 )
