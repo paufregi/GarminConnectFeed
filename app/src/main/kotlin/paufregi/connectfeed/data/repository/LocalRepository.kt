49bffb462ed7c3b939f8d42e864f69f5deead060
Paul Ellis <paul@movio.co>
2026-09-17 22:29:49 +1200

    Add LocalRepository to interface with DB

---
+++ app/src/main/kotlin/paufregi/connectfeed/data/repository/LocalRepository.kt
@@ -0,0 +1,40 @@
+package paufregi.connectfeed.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import paufregi.connectfeed.core.models.Gear
+import paufregi.connectfeed.core.models.Profile
+import paufregi.connectfeed.core.models.User
+import paufregi.connectfeed.data.database.GarminDao
+import paufregi.connectfeed.data.database.coverters.toCore
+import paufregi.connectfeed.data.database.coverters.toEntity
+import javax.inject.Inject
+
+class LocalRepository @Inject constructor(
+    private val garminDao: GarminDao,
+) {
+    fun getAllProfiles(user: User): Flow<List<Profile>> =
+        garminDao.getAllProfiles(user.id).map { profiles -> profiles.map { it.toCore() } }
+
+    suspend fun getProfile(id: Long): Profile? =
+        garminDao.getProfile(id)?.toCore()
+
+    suspend fun saveProfile(user: User, profile: Profile) =
+        garminDao.saveProfile(profile.toEntity(user.id))
+
+    suspend fun deleteProfile(user: User, profile: Profile) =
+        garminDao.deleteProfile(profile.toEntity(user.id))
+
+    fun getAllGears(user: User): Flow<List<Gear>> =
+        garminDao.getAllGears(user.id).map { gears -> gears.map { it.toCore() } }
+
+    suspend fun getGear(id: String): Gear? =
+        garminDao.getGear(id)?.toCore()
+
+    suspend fun saveGear(user: User, gear: Gear) =
+        garminDao.saveGear(gear.toEntity(user.id))
+
+    suspend fun deleteGear(user: User, gear: Gear) =
+        garminDao.deleteGear(gear.toEntity(user.id))
+}
+
---
+++ app/src/test/kotlin/paufregi/connectfeed/data/repository/LocalRepositoryTest.kt
@@ -0,0 +1,330 @@
+package paufregi.connectfeed.data.repository
+
+import app.cash.turbine.test
+import com.google.common.truth.Truth.assertThat
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.confirmVerified
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import paufregi.connectfeed.core.models.ActivityType
+import paufregi.connectfeed.core.models.Course
+import paufregi.connectfeed.core.models.EventType
+import paufregi.connectfeed.core.models.Gear
+import paufregi.connectfeed.core.models.GearType
+import paufregi.connectfeed.core.models.Profile
+import paufregi.connectfeed.data.database.GarminDao
+import paufregi.connectfeed.data.database.entities.GearEntity
+import paufregi.connectfeed.data.database.entities.ProfileEntity
+import paufregi.connectfeed.user
+
+class LocalRepositoryTest {
+
+    private lateinit var repo: LocalRepository
+    private val dao = mockk<GarminDao>()
+
+    @Before
+    fun setup(){
+        repo = LocalRepository(dao)
+    }
+
+    @After
+    fun tearDown(){
+        confirmVerified(dao)
+        clearAllMocks()
+    }
+
+    @Test
+    fun `Get all profiles`() = runTest {
+        val profiles = listOf(
+            Profile(
+                id = 1,
+                name = "profile 1",
+                eventType = EventType.Training,
+                type = ActivityType.Cycling,
+                course = Course(1, "course 1", 10234.00, ActivityType.Cycling),
+                water = 2
+            ),
+            Profile(
+                id = 2,
+                name = "profile 2",
+                eventType = EventType.Recreation,
+                type = ActivityType.Running,
+                course = Course(2, "course 2", 15007.00, ActivityType.Running),
+            )
+        )
+        val profileEntities = listOf(
+            ProfileEntity(
+                id = 1,
+                userId = user.id,
+                name = "profile 1",
+                eventType = EventType.Training,
+                type = ActivityType.Cycling,
+                course = Course(1, "course 1", 10234.00, ActivityType.Cycling),
+                water = 2
+            ),
+            ProfileEntity(
+                id = 2,
+                userId = user.id,
+                name = "profile 2",
+                eventType = EventType.Recreation,
+                type = ActivityType.Running,
+                course = Course(2, "course 2", 15007.00, ActivityType.Running),
+            )
+        )
+
+        coEvery { dao.getAllProfiles(any()) } returns flowOf(profileEntities)
+
+        val res = repo.getAllProfiles(user)
+
+        res.test {
+            assertThat(awaitItem()).isEqualTo(profiles)
+            cancelAndIgnoreRemainingEvents()
+        }
+
+        coVerify { dao.getAllProfiles(user.id) }
+    }
+
+    @Test
+    fun `Get all profile - no result`() = runTest {
+        coEvery { dao.getProfile(any()) } returns null
+
+        val res = repo.getProfile(1)
+
+        assertThat(res).isNull()
+
+        coVerify { dao.getProfile(1) }
+    }
+
+    @Test
+    fun `Get all profiles - empty list`() = runTest {
+        coEvery { dao.getAllProfiles(any()) } returns flowOf(emptyList<ProfileEntity>())
+
+        val res = repo.getAllProfiles(user)
+
+        res.test {
+            assertThat(awaitItem()).isEqualTo(emptyList<Profile>())
+            cancelAndIgnoreRemainingEvents()
+        }
+
+        coVerify { dao.getAllProfiles(user.id) }
+    }
+
+    @Test
+    fun `Get profile`() = runTest {
+        val profile = Profile(
+            id = 1,
+            name = "profile",
+            eventType = EventType.Training,
+            type = ActivityType.Cycling,
+            course = Course(1, "course", 10234.00, ActivityType.Cycling),
+            water = 2
+        )
+        val profileEntity = ProfileEntity(
+            id = 1,
+            userId = user.id,
+            name = "profile",
+            eventType = EventType.Training,
+            type = ActivityType.Cycling,
+            course = Course(1, "course", 10234.00, ActivityType.Cycling),
+            water = 2
+        )
+
+        coEvery { dao.getProfile(any()) } returns profileEntity
+
+        val res = repo.getProfile(1)
+
+        assertThat(res).isEqualTo(profile)
+
+        coVerify { dao.getProfile(1) }
+    }
+
+    @Test
+    fun `Save profile`() = runTest {
+        val profile = Profile(
+            name = "profile",
+            eventType = EventType.Training,
+            type = ActivityType.Cycling,
+            course = Course(1, "course", 10234.00, ActivityType.Cycling),
+            water = 2
+        )
+
+        val profileEntity  = ProfileEntity(
+            name = "profile",
+            userId = user.id,
+            eventType = EventType.Training,
+            type = ActivityType.Cycling,
+            course = Course(1, "course", 10234.00, ActivityType.Cycling),
+            water = 2
+        )
+
+        coEvery { dao.saveProfile(any()) } returns Unit
+
+        repo.saveProfile(user, profile)
+
+        coVerify { dao.saveProfile(profileEntity) }
+    }
+
+    @Test
+    fun `Delete profile`() = runTest {
+        val profile = Profile(
+            id = 1,
+            name = "profile",
+            eventType = EventType.Training,
+            type = ActivityType.Cycling,
+            course = Course(1, "course", 10234.00, ActivityType.Cycling),
+        )
+
+        val profileEntity  = ProfileEntity(
+            id = 1,
+            userId = user.id,
+            name = "profile",
+            eventType = EventType.Training,
+            type = ActivityType.Cycling,
+            course = Course(1, "course", 10234.00, ActivityType.Cycling),
+        )
+
+        coEvery { dao.deleteProfile(any()) } returns Unit
+
+        repo.deleteProfile(user, profile)
+
+        coVerify { dao.deleteProfile(profileEntity) }
+    }
+
+    @Test
+    fun `Get all gears`() = runTest {
+        val gears = listOf(
+            Gear(
+                id = "gear-1",
+                name = "gear 1",
+                type = GearType.Shoe,
+                distance = 1000,
+                stravaId = null
+            ),
+            Gear(
+                id = "gear-2",
+                name = "gear 2",
+                type = GearType.Bike,
+                distance = 2000,
+                stravaId = null
+            )
+        )
+        val gearEntities = listOf(
+            GearEntity(
+                id = "gear-1",
+                userId = user.id,
+                name = "gear 1",
+                type = GearType.Shoe,
+                distance = 1000
+            ),
+            GearEntity(
+                id = "gear-2",
+                userId = user.id,
+                name = "gear 2",
+                type = GearType.Bike,
+                distance = 2000
+            )
+        )
+
+        coEvery { dao.getAllGears(any()) } returns flowOf(gearEntities)
+
+        val res = repo.getAllGears(user)
+
+        res.test {
+            assertThat(awaitItem()).isEqualTo(gears)
+            cancelAndIgnoreRemainingEvents()
+        }
+
+        coVerify { dao.getAllGears(user.id) }
+    }
+
+    @Test
+    fun `Get gear`() = runTest {
+        val gear = Gear(
+            id = "gear-1",
+            name = "gear",
+            type = GearType.Shoe,
+            distance = 1000,
+            stravaId = null
+        )
+        val gearEntity = GearEntity(
+            id = "gear-1",
+            userId = user.id,
+            name = "gear",
+            type = GearType.Shoe,
+            distance = 1000,
+        )
+
+        coEvery { dao.getGear(any()) } returns gearEntity
+
+        val res = repo.getGear("gear-1")
+
+        assertThat(res).isEqualTo(gear)
+
+        coVerify { dao.getGear("gear-1") }
+    }
+
+    @Test
+    fun `Get gear - no result`() = runTest {
+        coEvery { dao.getGear(any()) } returns null
+
+        val res = repo.getGear("gear-1")
+
+        assertThat(res).isNull()
+
+        coVerify { dao.getGear("gear-1") }
+    }
+
+    @Test
+    fun `Save gear`() = runTest {
+        val gear = Gear(
+            id = "gear-1",
+            name = "gear",
+            type = GearType.Shoe,
+            distance = 1000,
+            stravaId = null
+        )
+        val gearEntity = GearEntity(
+            id = "gear-1",
+            userId = user.id,
+            name = "gear",
+            type = GearType.Shoe,
+            distance = 1000
+        )
+
+        coEvery { dao.saveGear(any()) } returns Unit
+
+        repo.saveGear(user, gear)
+
+        coVerify { dao.saveGear(gearEntity) }
+    }
+
+    @Test
+    fun `Delete gear`() = runTest {
+        val gear = Gear(
+            id = "gear-1",
+            name = "gear",
+            type = GearType.Shoe,
+            distance = 1000,
+            stravaId = null
+        )
+        val gearEntity = GearEntity(
+            id = "gear-1",
+            userId = user.id,
+            name = "gear",
+            type = GearType.Shoe,
+            distance = 1000
+        )
+
+        coEvery { dao.deleteGear(any()) } returns Unit
+
+        repo.deleteGear(user, gear)
+
+        coVerify { dao.deleteGear(gearEntity) }
+    }
+}
