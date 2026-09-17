ad6e0fa597b82eecb89334adb2a38e27b1c47d00
Paul Ellis <paul@movio.co>
2026-09-17 22:03:30 +1200

    Add API serializer for Instant from timestamp or epoc

---
+++ app/src/test/kotlin/paufregi/connectfeed/data/api/utils/serializers/TimestampSerializerTest.kt
@@ -0,0 +1,29 @@
+package paufregi.connectfeed.data.api.utils.serializers
+
+import com.google.common.truth.Truth.assertThat
+import kotlinx.serialization.json.Json
+import org.junit.Test
+import kotlin.time.Instant
+
+class TimestampSerializerTest {
+
+    val json = Json { encodeDefaults = true }
+    val dataString = "\"2025-01-01T01:00:00Z\""
+    val instant = Instant.parse(dataString)
+
+    @Test
+    fun `Serialize Instant to JSON`() {
+        val result = json.encodeToString(TimestampSerializer, instant)
+
+        assertThat(result).isEqualTo(dataString)
+    }
+
+    @Test
+    fun `Deserialize JSON string to Instant`() {
+        val result = json.decodeFromString(TimestampSerializer, dataString)
+
+        assertThat(result).isEqualTo(instant)
+    }
+}
+
+
