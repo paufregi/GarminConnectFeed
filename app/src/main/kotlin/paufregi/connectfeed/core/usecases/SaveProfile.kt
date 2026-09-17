f25cb4f7551c97c792c4a05020f9ae84f7df85d2
Paul Ellis <paul@movio.co>
2026-09-18 10:29:17 +1200

    Save Profile use-case

--- app/src/main/kotlin/paufregi/connectfeed/core/usecases/SaveProfile.kt
+++ app/src/main/kotlin/paufregi/connectfeed/core/usecases/SaveProfile.kt
@@ -2,23 +2,23 @@ package paufregi.connectfeed.core.usecases
 
 import kotlinx.coroutines.flow.firstOrNull
 import paufregi.connectfeed.core.models.Profile
+import paufregi.connectfeed.data.repository.AppRepository
 import paufregi.connectfeed.data.repository.AuthRepository
-import paufregi.connectfeed.data.repository.GarminRepository
 import javax.inject.Inject
 
 class SaveProfile @Inject constructor(
-    private val authRepository: AuthRepository,
-    private val garminRepository: GarminRepository
+    private val authRepo: AuthRepository,
+    private val repo: AppRepository
 ) {
-    suspend operator fun invoke(profile: Profile): Result<Unit> {
-        val user = authRepository.getUser().firstOrNull() ?: return Result.failure(Exception("User must be logged in"))
-        if (profile.name.isBlank()) return Result.failure(Exception("Name cannot be empty"))
-        if (profile.course != null) {
-            if (!profile.type.allowCourse) return Result.failure(Exception("Can't have course for ${profile.type.name} activity type"))
-            if (!profile.type.compatible(profile.course.type)) return Result.failure(Exception("Course not compatible with profile"))
-        }
+    suspend operator fun invoke(profile: Profile): Result<Unit> =
+        authRepo.getUser().firstOrNull()?.let { user ->
+            if (profile.name.isBlank()) return Result.failure(Exception("Name cannot be empty"))
+            profile.course?.let {
+                if (!profile.type.allowCourse) return Result.failure(Exception("Can't have course for ${profile.type.name} activity type"))
+                if (!profile.type.compatible(it.type)) return Result.failure(Exception("Course not compatible with profile"))
+            }
 
-        garminRepository.saveProfile(user, profile)
-        return Result.success(Unit)
-    }
+            repo.saveProfile(user, profile)
+            return Result.success(Unit)
+        } ?: Result.failure(Exception("User must be logged in"))
 }
--- app/src/test/kotlin/paufregi/connectfeed/core/usecases/SaveProfileTest.kt
+++ app/src/test/kotlin/paufregi/connectfeed/core/usecases/SaveProfileTest.kt
@@ -17,13 +17,13 @@ import paufregi.connectfeed.core.models.ActivityType
 import paufregi.connectfeed.core.models.Course
 import paufregi.connectfeed.core.models.EventType
 import paufregi.connectfeed.core.models.Profile
+import paufregi.connectfeed.data.repository.AppRepository
 import paufregi.connectfeed.data.repository.AuthRepository
-import paufregi.connectfeed.data.repository.GarminRepository
 import paufregi.connectfeed.user
 
 class SaveProfileTest{
     private val auth = mockk<AuthRepository>()
-    private val repo = mockk<GarminRepository>()
+    private val repo = mockk<AppRepository>()
     private lateinit var useCase: SaveProfile
 
     @Before
@@ -38,7 +38,7 @@ class SaveProfileTest{
     }
 
     @Test
-    fun `Save profile use-case`() = runTest {
+    fun `Save profile`() = runTest {
         val profile = Profile(
             id = 1,
             name = "Commute to home",
@@ -102,7 +102,7 @@ class SaveProfileTest{
     }
 
     @Test
-    fun `Invalid - Strength profile with course`() = runTest {
+    fun `Invalid - Course not allowed`() = runTest {
         val profile = Profile(
             id = 1,
             name = "Test",
@@ -123,28 +123,7 @@ class SaveProfileTest{
     }
 
     @Test
-    fun `Invalid - Any profile with course`() = runTest {
-        val profile = Profile(
-            id = 1,
-            name = "Test",
-            rename = true,
-            eventType = EventType.Training,
-            type = ActivityType.Any,
-            course = Course(id = 1, name = "course 1", distance = 10234.00, type = ActivityType.Cycling),
-            water = 550
-        )
-        every { auth.getUser() } returns flowOf(user)
-
-        val res = useCase(profile)
-
-        assertThat(res.isSuccess).isFalse()
-        assertThat(res.exceptionOrNull()?.message).isEqualTo("Can't have course for Any activity type")
-
-        verify { auth.getUser() }
-    }
-
-    @Test
-    fun `Invalid - Course not matching activity type`() = runTest {
+    fun `Invalid - Course not compatible with activity type`() = runTest {
         val profile = Profile(
             id = 1,
             name = "Test",
