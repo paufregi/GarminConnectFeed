f458b32b17720f085f74682344afa7754e3a7eaf
Paul Ellis <paul@movio.co>
2026-09-18 10:33:29 +1200

    Get Profiles use-case

--- app/src/main/kotlin/paufregi/connectfeed/core/usecases/GetProfiles.kt
+++ app/src/main/kotlin/paufregi/connectfeed/core/usecases/GetProfiles.kt
@@ -5,17 +5,17 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flowOf
 import paufregi.connectfeed.core.models.Profile
+import paufregi.connectfeed.data.repository.AppRepository
 import paufregi.connectfeed.data.repository.AuthRepository
-import paufregi.connectfeed.data.repository.GarminRepository
 import javax.inject.Inject
 
 class GetProfiles @Inject constructor(
-    private val authRepository: AuthRepository,
-    private val garminRepository: GarminRepository
+    private val authRepo: AuthRepository,
+    private val repo: AppRepository
 ) {
     @OptIn(ExperimentalCoroutinesApi::class)
     operator fun invoke(): Flow<List<Profile>> =
-        authRepository.getUser().flatMapMerge { user ->
-            user?.let { garminRepository.getAllProfiles(it) } ?: flowOf(emptyList())
+        authRepo.getUser().flatMapMerge { user ->
+            user?.let { repo.getAllProfiles(it) } ?: flowOf(emptyList())
         }
 }
