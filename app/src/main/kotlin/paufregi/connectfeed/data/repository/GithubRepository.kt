52faef64c16db44c638d6cfe685532e1ddf8728c
Paul Ellis <paul@movio.co>
2026-09-17 23:51:25 +1200

    Bring back GithubRepository

--- app/src/main/kotlin/paufregi/connectfeed/data/repository/GithubRepository.kt
+++ app/src/main/kotlin/paufregi/connectfeed/data/repository/GithubRepository.kt
@@ -1,11 +1,21 @@
 package paufregi.connectfeed.data.repository
 
+import paufregi.connectfeed.core.models.Release
+import paufregi.connectfeed.core.models.Version
 import paufregi.connectfeed.core.utils.mapOrFailure
 import paufregi.connectfeed.core.utils.toResult
 import paufregi.connectfeed.data.api.github.Github
+import javax.inject.Inject
 
-class GithubRepository(
-    private val github: Github
-) {
-    suspend fun getLatestRelease() = github.getLatestRelease().toResult().mapOrFailure { it.toCore() }
+class GithubRepository@Inject constructor(private val github: Github) {
+suspend fun getLatestRelease() =
+    github.getLatestRelease().toResult().mapOrFailure { r ->
+        val version = Version.parse(r.tagName)
+        val url = r.assets.find {
+            it.contentType == "application/vnd.android.package-archive" &&
+                it.downloadUrl.endsWith(".apk")
+        }?.downloadUrl
+
+        if (version != null && url != null) return@mapOrFailure Release(version, url)
+    }
 }
