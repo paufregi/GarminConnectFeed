d2de9b2bdf3c87838257c776648c5e193f5c08dd
Paul Ellis <paul@movio.co>
2026-09-19 08:44:33 +1200

    Update activity use case

--- app/src/main/kotlin/paufregi/connectfeed/core/usecases/UpdateActivity.kt
+++ app/src/main/kotlin/paufregi/connectfeed/core/usecases/UpdateActivity.kt
@@ -1,5 +1,7 @@
 package paufregi.connectfeed.core.usecases
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import paufregi.connectfeed.core.models.Activity
 import paufregi.connectfeed.core.models.Course
 import paufregi.connectfeed.core.models.EventType
@@ -7,13 +9,19 @@ import paufregi.connectfeed.core.models.Gear
 import paufregi.connectfeed.core.models.Workout
 import paufregi.connectfeed.core.utils.Formatter
 import paufregi.connectfeed.core.utils.failure
+import paufregi.connectfeed.core.utils.runCatchingResult
 import paufregi.connectfeed.data.repository.GarminRepository
+import paufregi.connectfeed.data.repository.StravaRepository
 import javax.inject.Inject
 
-class UpdateActivity @Inject constructor(private val garminRepository: GarminRepository) {
+class UpdateActivity @Inject constructor(
+    private val garminRepo: GarminRepository,
+    private val stravaRepo: StravaRepository,
+) {
     suspend operator fun invoke(
-        activity: Activity?,
+        activity: Activity,
         name: String?,
+        description: String?,
         eventType: EventType?,
         course: Course?,
         water: Int?,
@@ -21,20 +29,49 @@ class UpdateActivity @Inject constructor(private val garminRepository: GarminRep
         effort: Float?,
         workout: Workout?,
         gear: Gear?,
-    ): Result<Unit> {
-        if (activity == null || name == null || (course != null && !activity.type.allowCourse))
-            return Result.failure("Validation error")
+        trainingEffect: String?,
+        trainingEffectFlag: Boolean,
+    ): Result<Unit> = coroutineScope {
+        if (name == null || (course != null && !activity.type.allowCourse))
+            return@coroutineScope Result.failure("Validation error")
 
-        return garminRepository.updateActivity(
-            activity = activity,
-            name = name,
-            description = Formatter.workout(workout?.name),
-            eventType = eventType,
-            course = course,
-            water = water,
-            feel = feel,
-            effort = effort,
-            gear = gear,
-        )
+        val garminDeferred = async {
+            garminRepo.updateActivity(
+                activity = activity,
+                name = name,
+                description = Formatter.garminDescription(workout?.name),
+                eventType = eventType,
+                course = course,
+                water = water,
+                feel = feel,
+                effort = effort,
+                gear = gear,
+            )
+        }
+
+        val stravaDeferred = async {
+            activity.stravaId?.let {
+                stravaRepo.updateActivity(
+                    activity = activity,
+                    name = name,
+                    description = Formatter.stravaDescription(
+                        description = description,
+                        trainingEffect = trainingEffect.takeIf { trainingEffectFlag },
+                        workout = workout?.name,
+                    ),
+                    commute = eventType?.commute,
+                    gear = gear
+                )
+            } ?: Result.success(Unit)
+        }
+
+        val garminResult = runCatchingResult { garminDeferred.await() }
+        val stravaResult = runCatchingResult { stravaDeferred.await() }
+
+        if(garminResult.isFailure && stravaResult.isSuccess) return@coroutineScope Result.failure("Couldn't update Garmin activity")
+        if(garminResult.isSuccess && stravaResult.isFailure) return@coroutineScope Result.failure("Couldn't update Strava activity")
+        if(garminResult.isFailure && stravaResult.isFailure) return@coroutineScope Result.failure("Couldn't update Garmin & Strava activities")
+
+        Result.success(Unit)
     }
 }
--- app/src/test/kotlin/paufregi/connectfeed/core/usecases/UpdateActivityTest.kt
+++ app/src/test/kotlin/paufregi/connectfeed/core/usecases/UpdateActivityTest.kt
@@ -17,74 +17,147 @@ import paufregi.connectfeed.core.models.EventType
 import paufregi.connectfeed.core.models.Gear
 import paufregi.connectfeed.core.models.GearType
 import paufregi.connectfeed.core.models.Workout
+import paufregi.connectfeed.core.utils.failure
 import paufregi.connectfeed.data.repository.GarminRepository
+import paufregi.connectfeed.data.repository.StravaRepository
+import kotlin.time.Instant
 
 class UpdateActivityTest{
-    private val repo = mockk<GarminRepository>()
+    private val garminRepo = mockk<GarminRepository>()
+    private val stravaRepo = mockk<StravaRepository>()
     private lateinit var useCase: UpdateActivity
 
     val activity = Activity(
         id = 1,
         name = "name",
         type = ActivityType.Running,
+        eventType = EventType.Training,
         distance = 10234.00,
-        trainingEffect = "recovery"
+        trainingEffect = "recovery",
+        date = Instant.parse("2024-06-01T08:00:00Z"),
+        workoutId = 1L,
+        stravaId = 2L
     )
     val workout = Workout(1, "VO2 max")
     val name = "newName"
+    val description = "description"
     val eventType = EventType.Training
     val course = Course(id = 1, name = "courseName", distance = 1.0, type = ActivityType.Running)
     val water = 10
     val feel = 50f
     val effort = 90f
+    val trainingEffect = "VO2MAX"
     val gear = Gear(id = "shoe-1", name = "Shoe 1", type = GearType.Shoe)
 
     @Before
     fun setup(){
-        useCase = UpdateActivity(repo)
+        useCase = UpdateActivity(garminRepo, stravaRepo)
     }
 
     @After
     fun tearDown(){
-        confirmVerified(repo)
+        confirmVerified(garminRepo, stravaRepo)
         clearAllMocks()
     }
 
     @Test
     fun `Update activity`() = runTest {
-        coEvery { repo.updateActivity(any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns Result.success(Unit)
+        val activityNoStrava = activity.copy(stravaId = null)
+        val garminDescription = "Workout: VO₂ max"
 
-        val description = "Workout: VO₂ max"
-        val res = useCase(activity, name, eventType, course, water, feel, effort, workout, gear)
+        coEvery { garminRepo.updateActivity(any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns Result.success(Unit)
+
+        val res = useCase(activityNoStrava, name, null, eventType, course, water, feel, effort, workout, gear, null, false)
 
         assertThat(res.isSuccess).isTrue()
-        coVerify { repo.updateActivity(activity, name, description, eventType, course, water, feel, effort, gear) }
+        coVerify { garminRepo.updateActivity(activityNoStrava, name, garminDescription, eventType, course, water, feel, effort, gear) }
+    }
+
+    @Test
+    fun `Update activity - with Strava`() = runTest {
+        val garminDescription = "Workout: VO₂ max"
+        val stravaDescription = "description\n\nWorkout: VO₂ max\nBenefit: VO₂ max"
+
+        coEvery { garminRepo.updateActivity(any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns Result.success(Unit)
+        coEvery { stravaRepo.updateActivity(any(), any(), any(), any(), any()) } returns Result.success(Unit)
+
+        val res = useCase(activity, name, description, eventType, course, water, feel, effort, workout, gear, trainingEffect, true)
+
+        assertThat(res.isSuccess).isTrue()
+        coVerify {
+            garminRepo.updateActivity(activity, name, garminDescription, eventType, course, water, feel, effort, gear)
+            stravaRepo.updateActivity(activity, name, stravaDescription, eventType.commute, gear)
+        }
+    }
+
+    @Test
+    fun `Update activity - no description`() = runTest {
+        val garminDescription = "Workout: VO₂ max"
+        val stravaDescription = "Workout: VO₂ max\nBenefit: VO₂ max"
+        coEvery { garminRepo.updateActivity(any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns Result.success(Unit)
+        coEvery { stravaRepo.updateActivity(any(), any(), any(), any(), any()) } returns Result.success(Unit)
+
+        val res = useCase(activity, name, null, eventType, course, water, feel, effort, workout, gear, trainingEffect, true)
+
+        assertThat(res.isSuccess).isTrue()
+        coVerify {
+            garminRepo.updateActivity(activity, name, garminDescription, eventType, course, water, feel, effort, gear)
+            stravaRepo.updateActivity(activity, name, stravaDescription, eventType.commute, gear)
+        }
     }
 
     @Test
     fun `Update activity - no workout`() = runTest {
-        coEvery { repo.updateActivity(any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns Result.success(Unit)
+        val stravaDescription = "descriptionWorkout: VO₂ max\n\nBenefit: VO₂ max"
+        coEvery { garminRepo.updateActivity(any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns Result.success(Unit)
+        coEvery { stravaRepo.updateActivity(any(), any(), any(), any(), any()) } returns Result.success(Unit)
 
-        val res = useCase(activity, name, eventType, course, water, feel, effort, null, null)
+        val res = useCase(activity, name, description, eventType, course, water, feel, effort, null, gear, trainingEffect, true)
 
         assertThat(res.isSuccess).isTrue()
-        coVerify { repo.updateActivity(activity, name, null, eventType, course, water, feel, effort, null) }
+        coVerify {
+            garminRepo.updateActivity(activity, name, null, eventType, course, water, feel, effort, gear)
+            stravaRepo.updateActivity(activity, name, stravaDescription, eventType.commute, gear)
+        }
     }
 
     @Test
-    fun `Update activity - no gears`() = runTest {
-        coEvery { repo.updateActivity(any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns Result.success(Unit)
+    fun `Update activity - no training effect`() = runTest {
+        val garminDescription = "Workout: VO₂ max"
+        val stravaDescription = "description\n\nWorkout: VO₂ max"
 
-        val description = "Workout: VO₂ max"
-        val res = useCase(activity, name, eventType, course, water, feel, effort, workout, null)
+        coEvery { garminRepo.updateActivity(any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns Result.success(Unit)
+        coEvery { stravaRepo.updateActivity(any(), any(), any(), any(), any()) } returns Result.success(Unit)
+
+        val res = useCase(activity, name, description, eventType, course, water, feel, effort, workout, gear, null, true)
 
         assertThat(res.isSuccess).isTrue()
-        coVerify { repo.updateActivity(activity, name, description, eventType, course, water, feel, effort, null) }
+        coVerify {
+            garminRepo.updateActivity(activity, name, garminDescription, eventType, course, water, feel, effort, gear)
+            stravaRepo.updateActivity(activity, name, stravaDescription, eventType.commute, gear)
+        }
     }
 
     @Test
-    fun `Invalid - no activity`() = runTest {
-        val res = useCase(null, name, eventType, course, water, feel, effort, workout, null)
+    fun `Update activity - training effect flag false`() = runTest {
+        val garminDescription = "Workout: VO₂ max"
+        val stravaDescription = "description\n\nWorkout: VO₂ max"
+
+        coEvery { garminRepo.updateActivity(any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns Result.success(Unit)
+        coEvery { stravaRepo.updateActivity(any(), any(), any(), any(), any()) } returns Result.success(Unit)
+
+        val res = useCase(activity, name, description, eventType, course, water, feel, effort, workout, gear, trainingEffect, false)
+
+        assertThat(res.isSuccess).isTrue()
+        coVerify {
+            garminRepo.updateActivity(activity, name, garminDescription, eventType, course, water, feel, effort, gear)
+            stravaRepo.updateActivity(activity, name, stravaDescription, eventType.commute, gear)
+        }
+    }
+
+    @Test
+    fun `Invalid - no name`() = runTest {
+        val res = useCase(activity, null, description, eventType, course, water, feel, effort, workout, gear, trainingEffect, true)
 
         assertThat(res.isSuccess).isFalse()
         assertThat(res.exceptionOrNull()?.message).isEqualTo("Validation error")
@@ -92,18 +165,65 @@ class UpdateActivityTest{
 
     @Test
     fun `Invalid - course not allowed`() = runTest {
-        val activity = Activity(
-            id = 1,
-            name = "name",
-            type = ActivityType.Swimming,
-            distance = 1034.00,
-            trainingEffect = "recovery"
-        )
-        val course = Course(id = 1, name = "courseName", distance = 1.0, type = ActivityType.Running)
+        val invalidActivity = activity.copy(type = ActivityType.Swimming)
 
-        val res = useCase(activity, name, eventType, course, water, feel, effort, workout, null)
+        val res = useCase(invalidActivity, name, description, eventType, course, water, feel, effort, workout, gear, trainingEffect, true)
 
         assertThat(res.isSuccess).isFalse()
         assertThat(res.exceptionOrNull()?.message).isEqualTo("Validation error")
+    }
+
+    @Test
+    fun `Update activity - garmin failed`() = runTest {
+        val garminDescription = "Workout: VO₂ max"
+        val stravaDescription = "description\n\nWorkout: VO₂ max\nBenefit: VO₂ max"
+
+        coEvery { garminRepo.updateActivity(any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns Result.failure("error")
+        coEvery { stravaRepo.updateActivity(any(), any(), any(), any(), any()) } returns Result.success(Unit)
+
+        val res = useCase(activity, name, description, eventType, course, water, feel, effort, workout, gear, trainingEffect, true)
+
+        assertThat(res.isSuccess).isFalse()
+        assertThat(res.exceptionOrNull()?.message).isEqualTo("Couldn't update Garmin activity")
+        coVerify {
+            garminRepo.updateActivity(activity, name, garminDescription, eventType, course, water, feel, effort, gear)
+            stravaRepo.updateActivity(activity, name, stravaDescription, eventType.commute, gear)
+        }
+    }
+
+    @Test
+    fun `Update activity - strava failed`() = runTest {
+        val garminDescription = "Workout: VO₂ max"
+        val stravaDescription = "description\n\nWorkout: VO₂ max\nBenefit: VO₂ max"
+
+        coEvery { garminRepo.updateActivity(any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns Result.success(Unit)
+        coEvery { stravaRepo.updateActivity(any(), any(), any(), any(), any()) } returns Result.failure("error")
+
+        val res = useCase(activity, name, description, eventType, course, water, feel, effort, workout, gear, trainingEffect, true)
+
+        assertThat(res.isSuccess).isFalse()
+        assertThat(res.exceptionOrNull()?.message).isEqualTo("Couldn't update Strava activity")
+        coVerify {
+            garminRepo.updateActivity(activity, name, garminDescription, eventType, course, water, feel, effort, gear)
+            stravaRepo.updateActivity(activity, name, stravaDescription, eventType.commute, gear)
+        }
+    }
+
+    @Test
+    fun `Update activity - both failed`() = runTest {
+        val garminDescription = "Workout: VO₂ max"
+        val stravaDescription = "description\n\nWorkout: VO₂ max\nBenefit: VO₂ max"
+
+        coEvery { garminRepo.updateActivity(any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns Result.failure("error")
+        coEvery { stravaRepo.updateActivity(any(), any(), any(), any(), any()) } returns Result.failure("error")
+
+        val res = useCase(activity, name, description, eventType, course, water, feel, effort, workout, gear, trainingEffect, true)
+
+        assertThat(res.isSuccess).isFalse()
+        assertThat(res.exceptionOrNull()?.message).isEqualTo("Couldn't update Garmin & Strava activities")
+        coVerify {
+            garminRepo.updateActivity(activity, name, garminDescription, eventType, course, water, feel, effort, gear)
+            stravaRepo.updateActivity(activity, name, stravaDescription, eventType.commute, gear)
+        }
     }
 }
