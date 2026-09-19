31aaa0f11e3812f285d3428a57802a86f72adc66
Paul Ellis <paul@movio.co>
2026-09-19 19:48:23 +1200

    Android instrumented integration tests for Repositories

--- app/src/androidTest/kotlin/paufregi/connectfeed/data/repository/GarminRepositoryTest.kt
+++ app/src/androidTest/kotlin/paufregi/connectfeed/data/repository/GarminRepositoryTest.kt
@@ -1,8 +1,6 @@
 package paufregi.connectfeed.data.repository
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.datastore.preferences.core.edit
-import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -13,26 +11,21 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import paufregi.connectfeed.MockWebServerRule
+import paufregi.connectfeed.MockServer
 import paufregi.connectfeed.authToken
 import paufregi.connectfeed.connectDispatcher
 import paufregi.connectfeed.connectPort
-import paufregi.connectfeed.core.models.Gear
+import paufregi.connectfeed.core.models.EventType
 import paufregi.connectfeed.core.models.GearType
-import paufregi.connectfeed.core.models.Profile
 import paufregi.connectfeed.core.models.User
 import paufregi.connectfeed.core.models.Workout
-import paufregi.connectfeed.data.database.GarminDatabase
+import paufregi.connectfeed.data.api.garmin.models.Activity
+import paufregi.connectfeed.data.api.garmin.models.ActivityType
+import paufregi.connectfeed.data.api.garmin.models.Course
+import paufregi.connectfeed.data.api.garmin.models.Gear
 import paufregi.connectfeed.data.datastore.AuthStore
-import paufregi.connectfeed.data.datastore.StravaStore
 import paufregi.connectfeed.garminSSODispatcher
 import paufregi.connectfeed.garminSSOPort
-import paufregi.connectfeed.preAuthToken
-import paufregi.connectfeed.sslSocketFactory
-import paufregi.connectfeed.stravaAuthToken
-import paufregi.connectfeed.stravaDispatcher
-import paufregi.connectfeed.stravaPort
-import paufregi.connectfeed.user
 import java.io.File
 import javax.inject.Inject
 import kotlin.time.Instant
@@ -40,6 +33,7 @@ import paufregi.connectfeed.core.models.Activity as CoreActivity
 import paufregi.connectfeed.core.models.ActivityType as CoreActivityType
 import paufregi.connectfeed.core.models.Course as CoreCourse
 import paufregi.connectfeed.core.models.EventType as CoreEventType
+import paufregi.connectfeed.core.models.Gear as CoreGear
 
 @HiltAndroidTest
 class GarminRepositoryTest {
@@ -56,107 +50,55 @@ class GarminRepositoryTest {
     @Inject
     lateinit var authStore: AuthStore
 
-    @Inject
-    lateinit var stravaStore: StravaStore
-
-    @Inject
-    lateinit var database: GarminDatabase
-
-    @JvmField @Rule val connectServer = MockWebServerRule(connectPort, sslSocketFactory, connectDispatcher)
-    @JvmField @Rule val garminSSOServer = MockWebServerRule(garminSSOPort, sslSocketFactory, garminSSODispatcher)
-    @JvmField @Rule val stravaServer = MockWebServerRule(stravaPort, sslSocketFactory, stravaDispatcher)
+    @JvmField @Rule val connectServer = MockServer.createSecure(connectPort, connectDispatcher)
+    @JvmField @Rule val garminSSOServer = MockServer.createSecure(garminSSOPort, garminSSODispatcher)
 
     @Before
     fun setup() {
         hiltRule.inject()
+        runBlocking(Dispatchers.IO){
+            authStore.saveGarminToken(authToken)
+        }
     }
 
     @After
     fun tearDown() {
-        database.close()
         runBlocking(Dispatchers.IO){
-            authStore.dataStore.edit { it.clear() }
+            authStore.clear()
         }
     }
 
-
     @Test
-    fun `Fetch user`() = runTest {
-        authStore.savePreAuthToken(preAuthToken)
-        authStore.saveAuthToken(authToken)
-
+    fun `Get user profile`() = runTest {
         val expected = User(1, "Paul", "https://profile.image.com/large.jpg")
 
-        val res = repo.fetchUser()
+        val res = repo.getUserProfile()
 
         assertThat(res.isSuccess).isTrue()
         assertThat(res.getOrNull()).isEqualTo(expected)
     }
 
     @Test
-    fun `Store profiles`() = runTest {
-        val profile = Profile(id = 1, name = "test")
-        repo.saveProfile(user, profile)
-        assertThat(repo.getProfile(profile.id)).isEqualTo(profile)
-
-        repo.deleteProfile(user, profile)
-        assertThat(repo.getProfile(profile.id)).isNull()
-
-        repo.getAllProfiles(user).test{
-            assertThat(awaitItem()).isEmpty()
-            repo.saveProfile(user, profile)
-            assertThat(awaitItem()).containsExactly(profile)
-            cancelAndIgnoreRemainingEvents()
-        }
-    }
-
-    @Test
-    fun `Store gears`() = runTest {
-        val gear = Gear(
-            id = "gear-1",
-            name = "gear1",
-            type = GearType.Shoe,
-            distance = 1000
-        )
-
-        repo.saveGear(user, gear)
-        assertThat(repo.getGear(gear.id)).isEqualTo(gear)
-
-        repo.deleteGear(user, gear)
-        assertThat(repo.getGear(gear.id)).isNull()
-
-        repo.getAllGears(user).test {
-            assertThat(awaitItem()).isEmpty()
-            repo.saveGear(user, gear)
-            assertThat(awaitItem()).containsExactly(gear)
-            cancelAndIgnoreRemainingEvents()
-        }
-    }
-
-    @Test
     fun `Get activities`() = runTest {
-        authStore.savePreAuthToken(preAuthToken)
-        authStore.saveAuthToken(authToken)
-
         val expected = listOf(
-            CoreActivity(
+            Activity(
                 id = 1,
                 name = "Activity 1",
-                distance = 17804.00,
-                trainingEffect = "recovery",
-                type = CoreActivityType.RoadBiking,
+                type = ActivityType(10, "road_bike", CoreActivityType.RoadBiking),
                 eventType = CoreEventType.Transportation,
-                date = Instant.fromEpochMilliseconds(1729754100000),
+                distance = 17804.00,
+                trainingEffectLabel = "RECOVERY",
+                beginTimestamp = Instant.fromEpochMilliseconds(1729754100000),
                 workoutId = 1
             ),
-            CoreActivity(
+            Activity(
                 id = 2,
                 name = "Activity 2",
-                distance = 17760.00,
-                trainingEffect = "recovery",
-                type = CoreActivityType.RoadBiking,
+                type = ActivityType(10, "road_bike", CoreActivityType.RoadBiking),
                 eventType = CoreEventType.Transportation,
-                date = Instant.fromEpochMilliseconds(1729705968000),
+                distance = 17760.00,
+                trainingEffectLabel = "RECOVERY",
+                beginTimestamp = Instant.fromEpochMilliseconds(1729705968000),
                 workoutId = 2
             )
         )
@@ -168,27 +110,27 @@ class GarminRepositoryTest {
     }
 
     @Test
-    fun `Get Strava activities`() = runTest {
-        stravaStore.saveToken(stravaAuthToken)
-
+    fun `Get gears`() = runTest {
         val expected = listOf(
-            CoreActivity(
-                id = 1,
-                name = "Happy Friday",
-                distance = 7804.0,
-                type = CoreActivityType.StravaRunning,
-                date = Instant.parse("2018-05-02T12:15:09Z")
+            Gear(
+                id = "522f1e21-0822-451d-88a3-b0b661802c2f",
+                brand = "Mizuno",
+                model = "Neo Vista",
+                name = null,
+                type = GearType.Shoe,
+                distance = 51955.4501953125
             ),
-            CoreActivity(
-                id = 2,
-                name = "Bondcliff",
-                distance = 23676.0,
-                type = CoreActivityType.StravaRide,
-                date = Instant.parse("2024-10-24T07:15:30Z")
-                )
+            Gear(
+                id = "789dccf8-b669-4903-bf46-7d8d9369124e",
+                brand = "Giant",
+                model = "Contend AR",
+                name = "Nova",
+                type = GearType.Bike,
+                distance = 17226955.28363037
+            ),
         )
 
-        val res = repo.getStravaActivities(5)
+        val res = repo.getGears()
 
         assertThat(res.isSuccess).isTrue()
         assertThat(res.getOrNull()).isEqualTo(expected)
@@ -196,12 +138,9 @@ class GarminRepositoryTest {
 
     @Test
     fun `Get courses`() = runTest {
-        authStore.savePreAuthToken(preAuthToken)
-        authStore.saveAuthToken(authToken)
-
         val expected = listOf(
-            CoreCourse(id = 1, name = "Course 1", distance = 10235.00, type = CoreActivityType.Running),
-            CoreCourse(id = 2, name = "Course 2", distance = 15008.00, type = CoreActivityType.RoadBiking),
+            Course(id = 1, name = "Course 1", distance = 10235.00, type = ActivityType(10, "running", CoreActivityType.Running)),
+            Course(id = 2, name = "Course 2", distance = 15008.00, type = ActivityType(10, "road_bike", CoreActivityType.RoadBiking)),
         )
 
         val res = repo.getCourses()
@@ -212,9 +151,6 @@ class GarminRepositoryTest {
 
     @Test
     fun `Get workout`() = runTest {
-        authStore.savePreAuthToken(preAuthToken)
-        authStore.saveAuthToken(authToken)
-
         val expected = Workout(1, "Power - Zone 6")
 
         val res = repo.getWorkout(1)
@@ -224,38 +160,14 @@ class GarminRepositoryTest {
     }
 
     @Test
-    fun `Get gears`() = runTest {
-        authStore.savePreAuthToken(preAuthToken)
-        authStore.saveAuthToken(authToken)
-
-        val expected = listOf(
-            Gear(
-                id = "522f1e21-0822-451d-88a3-b0b661802c2f",
-                name = "Mizuno Neo Vista",
-                type = GearType.Shoe,
-                distance = 51955
-            ),
-            Gear(
-                id = "789dccf8-b669-4903-bf46-7d8d9369124e",
-                name = "Nova",
-                type = GearType.Bike,
-                distance = 17226955
-            ),
-        )
-
-        val res = repo.getGears()
-
-        assertThat(res.isSuccess).isTrue()
-        assertThat(res.getOrNull()).isEqualTo(expected)
-    }
-
-
-    @Test
     fun `Update activity`() = runTest {
-        authStore.savePreAuthToken(preAuthToken)
-        authStore.saveAuthToken(authToken)
-
-        val activity = CoreActivity(id = 1, name = "activity", distance = 17803.00, trainingEffect = "", type = CoreActivityType.Cycling)
+        val activity = CoreActivity(
+            id = 1,
+            name = "activity",
+            type = CoreActivityType.Cycling,
+            eventType = EventType.Training,
+            date = Instant.parse("2024-10-24T07:15:30Z"),
+        )
         val name = "newName"
         val description = "newDescription"
         val eventType = CoreEventType.Training
@@ -263,7 +175,7 @@ class GarminRepositoryTest {
         val water = 2
         val effort = 50f
         val feel = 80f
-        val gear = Gear("bike-1", "bike", GearType.Bike, 1000)
+        val gear = CoreGear("bike-1", "bike", GearType.Bike, 1000)
 
         val res = repo.updateActivity(
             activity = activity,
@@ -281,38 +193,7 @@ class GarminRepositoryTest {
     }
 
     @Test
-    fun `Update strava activity`() = runTest {
-        stravaStore.saveToken(stravaAuthToken)
-
-        val activity = CoreActivity(id = 1, name = "activity", distance = 17803.00, trainingEffect = "", type = CoreActivityType.Cycling)
-        val name = "newName"
-        val description = "description"
-        val commute = true
-
-        val res = repo.updateStravaActivity(
-            activity = activity,
-            name = name,
-            description = description,
-            commute = commute
-        )
-
-        assertThat(res.isSuccess).isTrue()
-    }
-
-    @Test
-    fun `Update strava profile`() = runTest {
-        stravaStore.saveToken(stravaAuthToken)
-
-        val res = repo.updateStravaProfile(75.6f)
-
-        assertThat(res.isSuccess).isTrue()
-    }
-
-    @Test
     fun `Upload file`() = runTest {
-        authStore.savePreAuthToken(preAuthToken)
-        authStore.saveAuthToken(authToken)
-
         val testFile = File.createTempFile("test", "test")
         testFile.deleteOnExit()
         val res = repo.uploadFile(testFile)
